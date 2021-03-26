b0a668f53ea732d59b418aecef661d215bc7e760
Macesuted <macesuted@qq.com>
2021-03-26 17:38:39 +0800

    fix

--- packages/hydrooj/src/handler/import.syzoj.ts
+++ packages/hydrooj/src/handler/import.syzoj.ts
@@ -139,7 +139,7 @@ class ProblemImportSYZOJHandler extends Handler {
             .send({
                 displayId: +pid,
                 localizedContentsOfAllLocales: true,
-                tagsOfLocale: tagsOfLocale,
+                tagsOfLocale,
                 samples: true,
                 testData: true,
                 additionalFiles: true,
