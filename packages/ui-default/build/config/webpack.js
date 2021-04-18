1968f03aa053fabba1378fc46fb2e0b4752ea5cc
undefined <i@undefined.moe>
2021-04-18 12:38:39 +0800

    add public to eslintignore

--- packages/ui-default/build/config/webpack.js
+++ packages/ui-default/build/config/webpack.js
@@ -143,7 +143,7 @@ export default function (env = {}) {
         patterns: [
           { from: root('static') },
           { from: root(`${dirname(require.resolve('emojify.js/package.json'))}/dist/images/basic`), to: 'img/emoji/' },
-          { from: root(`${dirname(require.resolve('vditor/package.json'))}`), to: 'vditor' },
+          { from: root(`${dirname(require.resolve('vditor/package.json'))}`), to: 'vditor/' },
         ],
       }),
       new webpack.DefinePlugin({
