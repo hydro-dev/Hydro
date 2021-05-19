c228f07464cf05e49ed86762a4b74877de5ed2b1
wuxianucw <x-wuxian@qq.com>
2021-05-19 10:03:13 +0800

    ui: try to fix unexpected drop

--- packages/ui-default/pages/problem_files.page.js
+++ packages/ui-default/pages/problem_files.page.js
@@ -208,7 +208,7 @@ const page = new NamedPage('problem_files', () => {
         files.push(ev.dataTransfer.files[i]);
       }
     }
-    handleClickUpload(type, files);
+    if (file.length > 0) handleClickUpload(type, files);
   }
 
   async function startEdit(filename, value) {
