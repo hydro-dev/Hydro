561c4d41751d25cdf37d0b00062667b563b536c4
undefined <i@undefined.moe>
2021-05-21 17:46:29 +0800

    office: also support slide & cell

--- packages/onlyoffice/public/office.page.js
+++ packages/onlyoffice/public/office.page.js
@@ -3,17 +3,22 @@
     const { $ } = window.node_modules;
     const { request } = window.Hydro.utils;
 
+    let loaded = false;
+    async function load() {
+        if (loaded) return Promise.resolve();
+        return new Promise((resolve) => {
+            const scriptElement = document.createElement('script');
+            scriptElement.src = UiContext.onlyofficeApi;
+            scriptElement.async = true;
+            document.head.appendChild(scriptElement);
+            scriptElement.onload = resolve;
+            loaded = true;
+        });
+    }
+
     const page = new AutoloadPage('onlyoffice', async () => {
         const res = $('div[data-doc]');
-        if (res.length) {
-            await new Promise((resolve) => {
-                const scriptElement = document.createElement('script');
-                scriptElement.src = UiContext.onlyofficeApi;
-                scriptElement.async = true;
-                document.head.appendChild(scriptElement);
-                scriptElement.onload = resolve;
-            });
-        }
+        if (res.length) await load();
         res.get().forEach(async (element) => {
             const id = `doc-${Math.random().toString()}`;
             $(element).attr('id', id);
@@ -33,6 +38,58 @@
                 editorConfig: {
                     lang,
                     mode: 'view',
+                },
+                documentType: 'word',
+                height: '980px',
+            });
+        });
+        res = $('div[data-cell]');
+        if (res.length) await load();
+        res.get().forEach(async (element) => {
+            const id = `cell-${Math.random().toString()}`;
+            $(element).attr('id', id);
+            const url = $(element).text();
+            const t = new URL(url, window.location.href).pathname.split('.');
+            const n = new URL(url, window.location.href).pathname.split('/');
+            const res = await request.get(url);
+            const lang = UserContext.viewLang.includes('_') ? UserContext.viewLang.split('_')[0] : UserContext.viewLang;
+            // eslint-disable-next-line no-undef
+            window.editor = new DocsAPI.DocEditor(id, {
+                document: {
+                    fileType: t[t.length - 1],
+                    key: Math.random().toString(16),
+                    title: n[n.length - 1],
+                    url: new URL(res.url, window.location.href),
+                },
+                editorConfig: {
+                    lang,
+                    mode: 'cell',
+                },
+                documentType: 'word',
+                height: '980px',
+            });
+        });
+        res = $('div[data-slide]');
+        if (res.length) await load();
+        res.get().forEach(async (element) => {
+            const id = `slide-${Math.random().toString()}`;
+            $(element).attr('id', id);
+            const url = $(element).text();
+            const t = new URL(url, window.location.href).pathname.split('.');
+            const n = new URL(url, window.location.href).pathname.split('/');
+            const res = await request.get(url);
+            const lang = UserContext.viewLang.includes('_') ? UserContext.viewLang.split('_')[0] : UserContext.viewLang;
+            // eslint-disable-next-line no-undef
+            window.editor = new DocsAPI.DocEditor(id, {
+                document: {
+                    fileType: t[t.length - 1],
+                    key: Math.random().toString(16),
+                    title: n[n.length - 1],
+                    url: new URL(res.url, window.location.href),
+                },
+                editorConfig: {
+                    lang,
+                    mode: 'slide',
                 },
                 documentType: 'word',
                 height: '980px',
