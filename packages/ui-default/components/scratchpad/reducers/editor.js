9b9243a87c51d6e30a1fbdf914a39e5dbc0b0cf4
undefined <i@undefined.moe>
2021-05-22 13:24:21 +0800

    ui: save editor state to localstorage

--- packages/ui-default/components/scratchpad/reducers/editor.js
+++ packages/ui-default/components/scratchpad/reducers/editor.js
@@ -1,21 +1,22 @@
 export default function reducer(state = {
   lang: UiContext.codeLang,
-  code: UiContext.codeTemplate,
+  code: localStorage.getItem(`${UiContext.pdoc.domainId}/${UiContext.pdoc.docId}`) || UiContext.codeTemplate,
 }, action) {
   switch (action.type) {
-  case 'SCRATCHPAD_EDITOR_UPDATE_CODE': {
-    return {
-      ...state,
-      code: action.payload,
-    };
-  }
-  case 'SCRATCHPAD_EDITOR_SET_LANG': {
-    return {
-      ...state,
-      lang: action.payload,
-    };
-  }
-  default:
-    return state;
+    case 'SCRATCHPAD_EDITOR_UPDATE_CODE': {
+      localStorage.setItem(`${UiContext.pdoc.domainId}/${UiContext.pdoc.docId}`, action.payload);
+      return {
+        ...state,
+        code: action.payload,
+      };
+    }
+    case 'SCRATCHPAD_EDITOR_SET_LANG': {
+      return {
+        ...state,
+        lang: action.payload,
+      };
+    }
+    default:
+      return state;
   }
 }
