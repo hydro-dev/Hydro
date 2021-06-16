71ebb3ffd6bad019748a2bc4dab5fc63033f2fef
undefined <i@undefined.moe>
2021-06-16 19:14:36 +0800

    judge: 更改解释型语言编译阶段写入位置

--- packages/hydrojudge/src/compile.ts
+++ packages/hydrojudge/src/compile.ts
@@ -9,9 +9,8 @@ export = async function compile(
     lang: LangConfig, code: string, target: string, copyIn: any = {}, next?: Function,
 ): Promise<Execute> {
     target = lang.target || target;
-    const f = {};
+    copyIn[lang.code_file] = { content: code };
     if (lang.compile) {
-        copyIn[lang.code_file] = { content: code };
         const {
             status, stdout, stderr, fileIds,
         } = await run(
@@ -21,13 +20,14 @@ export = async function compile(
         if (status !== STATUS.STATUS_ACCEPTED) throw new CompileError({ status, stdout, stderr });
         if (!fileIds[target]) throw new CompileError({ stderr: 'Executable file not found.' });
         if (next) next({ compiler_text: compilerText(stdout, stderr) });
-        f[target] = { fileId: fileIds[target] };
         return {
-            execute: lang.execute, copyIn: { ...copyIn, ...f }, clean: () => del(fileIds[target]), time: lang.time_limit_rate || 1,
+            execute: lang.execute,
+            copyIn: { ...copyIn, [target]: { fileId: fileIds[target] } },
+            clean: () => del(fileIds[target]),
+            time: lang.time_limit_rate || 1,
         };
     }
-    f[target] = { content: code };
     return {
-        execute: lang.execute, copyIn: { ...copyIn, ...f }, clean: () => Promise.resolve(null), time: lang.time_limit_rate || 1,
+        execute: lang.execute, copyIn, clean: () => Promise.resolve(null), time: lang.time_limit_rate || 1,
     };
 };
