5f41961fdb7e82fbdffc7c9f7e05c2050df53e1b
undefined <i@undefined.moe>
2021-07-09 20:00:04 +0800

    ui: hide domain-limited languages

--- packages/ui-default/handler.js
+++ packages/ui-default/handler.js
@@ -20,11 +20,12 @@ class WikiHelpHandler extends Handler {
     this.noCheckPermView = true;
   }
 
-  async get() {
+  async get({ domainId }) {
     const LANGS = setting.langs;
     const languages = {};
     // eslint-disable-next-line guard-for-in
     for (const key in LANGS) {
+      if (LANGS[key].domain && !LANGS[key].domain.includes(domainId)) continue;
       languages[`${LANGS[key].display}(${key})`] = LANGS[key].compile || LANGS[key].execute;
     }
     this.response.body = { languages };
