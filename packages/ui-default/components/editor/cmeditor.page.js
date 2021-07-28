cca54dc92a495505720ca1afd9fd8cdc63e6e76e
undefined <i@undefined.moe>
2021-07-29 00:40:10 +0800

    ui: add markup

--- packages/ui-default/components/editor/cmeditor.page.js
+++ packages/ui-default/components/editor/cmeditor.page.js
@@ -9,6 +9,9 @@ function runSubstitute($container) {
   $container.find('textarea[data-yaml]').get().forEach((element) => {
     CmEditor.getOrConstruct($(element), { language: 'yaml' });
   });
+  $container.find('textarea[data-json]').get().forEach((element) => {
+    CmEditor.getOrConstruct($(element), { language: 'json' });
+  });
   $container.find('textarea[data-plain]').get().forEach((element) => {
     CmEditor.getOrConstruct($(element), { language: 'plain' });
   });
