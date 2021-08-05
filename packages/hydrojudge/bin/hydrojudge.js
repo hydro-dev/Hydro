aabc4c432f27ea5cdd5a6d15dc203008d48e48e7
undefined <i@undefined.moe>
2021-08-06 01:16:16 +0800

    core: fix p-queue esm load

--- packages/hydrojudge/bin/hydrojudge.js
+++ packages/hydrojudge/bin/hydrojudge.js
@@ -2,31 +2,42 @@
 
 const argv = require('cac')().parse();
 const esbuild = require('esbuild');
+const fs = require('fs-extra');
+
+const major = +process.version.split('.')[0].split('v')[1];
+const minor = +process.version.split('.')[1];
 
 let transformTimeUsage = 0;
+let transformCount = 0;
 let displayTimeout;
-function transform(type, filename) {
+function transform(filename) {
     const start = new Date();
     const result = esbuild.buildSync({
         entryPoints: [filename],
         sourcemap: 'inline',
         platform: 'node',
         format: 'cjs',
-        target: `node${process.version.split('.')[0].split('v')[1]}`,
+        target: `node${major}.${minor}`,
         jsx: 'transform',
         write: false,
     });
     if (result.warnings.length) console.warn(result.warnings);
     transformTimeUsage += new Date().getTime() - start.getTime();
+    transformCount++;
     if (displayTimeout) clearTimeout(displayTimeout);
-    displayTimeout = setTimeout(() => console.log(`Code transform took ${transformTimeUsage}ms`), 1000);
+    displayTimeout = setTimeout(() => console.log(`Transformed ${transformCount} files. (${transformTimeUsage}ms)`), 1000);
     return result.outputFiles[0].text;
 }
-require.extensions['.ts'] = function loader(module, filename) {
-    return module._compile(transform('ts', filename), filename);
+const ESM = ['p-queue', 'p-timeout'];
+require.extensions['.js'] = function loader(module, filename) {
+    if (ESM.filter((i) => filename.includes(i)).length || major < 14) {
+        return module._compile(transform(filename), filename);
+    }
+    const content = fs.readFileSync(filename, 'utf-8');
+    return module._compile(content, filename);
 };
-require.extensions['.tsx'] = function loader(module, filename) {
-    return module._compile(transform('tsx', filename), filename);
+require.extensions['.ts'] = require.extensions['.tsx'] = function loader(module, filename) {
+    return module._compile(transform(filename), filename);
 };
 
 if (argv.options.debug) process.env.DEV = 'on';
--- packages/hydrooj/bin/hydrooj.js
+++ packages/hydrooj/bin/hydrooj.js
@@ -16,7 +16,7 @@ const minor = +process.version.split('.')[1];
 let transformTimeUsage = 0;
 let transformCount = 0;
 let displayTimeout;
-function transform(type, filename) {
+function transform(filename) {
     const start = new Date();
     const result = esbuild.buildSync({
         entryPoints: [filename],
@@ -34,16 +34,16 @@ function transform(type, filename) {
     displayTimeout = setTimeout(() => console.log(`Transformed ${transformCount} files. (${transformTimeUsage}ms)`), 1000);
     return result.outputFiles[0].text;
 }
-if (major < 14) {
-    require.extensions['.js'] = function loader(module, filename) {
-        return module._compile(transform('js', filename), filename);
-    };
-}
-require.extensions['.ts'] = function loader(module, filename) {
-    return module._compile(transform('ts', filename), filename);
+const ESM = ['p-queue', 'p-timeout'];
+require.extensions['.js'] = function loader(module, filename) {
+    if (ESM.filter((i) => filename.includes(i)).length || major < 14) {
+        return module._compile(transform(filename), filename);
+    }
+    const content = fs.readFileSync(filename, 'utf-8');
+    return module._compile(content, filename);
 };
-require.extensions['.tsx'] = function loader(module, filename) {
-    return module._compile(transform('tsx', filename), filename);
+require.extensions['.ts'] = require.extensions['.tsx'] = function loader(module, filename) {
+    return module._compile(transform(filename), filename);
 };
 
 function buildUrl(opts) {
