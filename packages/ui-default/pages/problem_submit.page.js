63894f99596f13108fb76468293e0301408ac9e3
undefined <i@undefined.moe>
2021-08-06 16:34:08 +0800

    ui: problem_submit

--- packages/ui-default/pages/problem_submit.page.js
+++ packages/ui-default/pages/problem_submit.page.js
@@ -21,9 +21,13 @@ const page = new NamedPage(['problem_submit', 'contest_detail_problem_submit', '
       if (key.startsWith(`${this.value}.`) && key !== this.value) options[key] = window.LANGS[key].display;
     }
     setOptions($('#codelang-sub-select'), options);
-    if (Object.keys(options).length) $('#codelang-sub-container').show();
-    else $('#codelang-sub-container').hide();
-    if (update) $('[name="lang"]').val($('#codelang-sub-select').val());
+    if (Object.keys(options).length) {
+      $('#codelang-sub-container').show();
+      if (update) $('[name="lang"]').val($('#codelang-sub-select').val());
+    } else {
+      $('#codelang-sub-container').hide();
+      if (update) $('[name="lang"]').val(this.value);
+    }
     return Object.keys(options)[0];
   }
   const main = {};
