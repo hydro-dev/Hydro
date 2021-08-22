b19c40d7e6618d87902c2946b8e700fb4b82d592
无限UCW <x-wuxian@qq.com>
2021-08-22 19:50:52 +0800

    ui: autocomplete: components

---
+++ packages/ui-default/components/autocomplete/components/DomainSelectAutoComplete.jsx
@@ -0,0 +1,67 @@
+import React, { forwardRef } from 'react';
+import PropTypes from 'prop-types';
+import request from 'vj/utils/request';
+import AutoComplete from './AutoComplete';
+
+const DomainSelectAutoComplete = forwardRef(function DomainSelectAutoComplete(props, ref) {
+    const itemsFn = (query) => {
+        return request.get('/domain/search', { q: query });
+    };
+
+    const itemText = (domain) => {
+        return domain.name;
+    };
+
+    const itemKey = (domain) => {
+        return domain._id;
+    };
+
+    const renderItem = (domain) => {
+        return (
+            <div className="media">
+                <div className="media__left medium">
+                    <img className="small domain-profile-avatar" src={domain.avatarUrl} width="30" height="30" />
+                </div>
+                <div className="media__body medium">
+                    <div className="domain-select__name">{domain.name}</div>
+                    <div className="domain-select__id">ID = {domain._id}</div>
+                </div>
+            </div>
+        );
+    };
+
+    return (
+        <AutoComplete
+            ref={ref}
+            itemsFn={itemsFn}
+            itemText={itemText}
+            itemKey={itemKey}
+            renderItem={renderItem}
+            {...props}
+        />
+    );
+});
+
+DomainSelectAutoComplete.propTypes = {
+    width: PropTypes.string,
+    height: PropTypes.string,
+    name: PropTypes.string,
+    listStyle: PropTypes.object,
+    multi: PropTypes.bool,
+    defaultItems: PropTypes.arrayOf(PropTypes.object),
+    allowEmptyQuery: PropTypes.bool,
+};
+
+DomainSelectAutoComplete.defaultProps = {
+    width: '100%',
+    height: 'auto',
+    name: '',
+    listStyle: {},
+    multi: false,
+    defaultItems: [],
+    allowEmptyQuery: false,
+};
+
+DomainSelectAutoComplete.displayName = 'DomainSelectAutoComplete';
+
+export default DomainSelectAutoComplete;
