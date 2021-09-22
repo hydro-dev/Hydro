1140f85654d66df17c1e600b0e082c0412eba4c2
undefined <i@undefined.moe>
2021-09-22 13:27:23 +0800

    core: trim config

--- packages/hydrooj/src/options.ts
+++ packages/hydrooj/src/options.ts
@@ -9,9 +9,9 @@ const logger = new Logger('options');
 export = function load() {
     const envFile = path.resolve(os.homedir(), '.hydro', 'env');
     if (fs.existsSync(envFile)) {
-        const content = fs.readFileSync(envFile).toString();
+        const content = fs.readFileSync(envFile).toString().replace(/\r/g, '');
         for (const line of content.split('\n')) {
-            process.env[line.split('=')[0]] = line.split('=')[1];
+            process.env[line.split('=')[0]] = line.split('=')[1].trim();
         }
     }
     const f = findFileSync('config.json', false);
