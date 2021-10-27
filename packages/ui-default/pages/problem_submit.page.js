dbe52e5016d49123510b05a1d242070069b58862
undefined <i@undefined.moe>
2021-10-27 17:20:10 +0800

    ui: fix lang limit

--- packages/ui-default/pages/problem_submit.page.js
+++ packages/ui-default/pages/problem_submit.page.js
@@ -8,13 +8,11 @@ function setOptions($el, options) {
 }
 
 const page = new NamedPage(['problem_submit', 'contest_detail_problem_submit', 'homework_detail_problem_submit'], async () => {
-  const { domainId } = UiContext.pdocConfig;
-
   function onChangeMain(update = true) {
     const options = {};
     for (const key in window.LANGS) {
       if (UiContext.pdocConfig.langs && !UiContext.pdocConfig.langs.includes(key)) continue;
-      if (window.LANGS[key].domain && !window.LANGS[key].domain.includes(domainId)) continue;
+      if (window.LANGS[key].domain && !window.LANGS[key].domain.includes(UiContext.domainId)) continue;
       if (key.startsWith(`${this.value}.`) && key !== this.value) options[key] = window.LANGS[key].display;
     }
     setOptions($('#codelang-sub-select'), options);
@@ -30,7 +28,7 @@ const page = new NamedPage(['problem_submit', 'contest_detail_problem_submit', '
   const main = {};
   for (const key in window.LANGS) {
     if (UiContext.pdocConfig.langs && !UiContext.pdocConfig.langs.filter((i) => i === key || i.startsWith(`${key}.`)).length) continue;
-    if (window.LANGS[key].domain && !window.LANGS[key].domain.includes(domainId)) continue;
+    if (window.LANGS[key].domain && !window.LANGS[key].domain.includes(UiContext.domainId)) continue;
     if (!key.includes('.')) main[key] = window.LANGS[key].display;
   }
   setOptions($('#codelang-main-select'), main);
