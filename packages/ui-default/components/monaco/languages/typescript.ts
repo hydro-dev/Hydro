785dcc162854f8bd6cb534101efe39e5151036f4
undefined <i@undefined.moe>
2021-12-08 23:50:46 +0800

    ui: set module to esnext to allow top-level-await

--- packages/ui-default/components/monaco/languages/typescript.ts
+++ packages/ui-default/components/monaco/languages/typescript.ts
@@ -2,26 +2,29 @@ import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 
 const types = require.context('!!raw-loader!@types/node/', true, /\.d\.ts$/);
 
-monaco.languages.typescript.javascriptDefaults.setDiagnosticsOptions({
+const diagnosticsOptions = {
   noSemanticValidation: false,
   noSyntaxValidation: false,
-});
-monaco.languages.typescript.javascriptDefaults.setCompilerOptions({
+};
+const compilerOptions = {
   target: monaco.languages.typescript.ScriptTarget.ES2020,
+  module: monaco.languages.typescript.ModuleKind.ESNext,
   allowNonTsExtensions: true,
-});
+};
+monaco.languages.typescript.javascriptDefaults.setDiagnosticsOptions(diagnosticsOptions);
+monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions(diagnosticsOptions);
+monaco.languages.typescript.javascriptDefaults.setCompilerOptions(compilerOptions);
+monaco.languages.typescript.typescriptDefaults.setCompilerOptions(compilerOptions);
 const libSource = [
   'declare function readline(): string;',
   'declare function print(content: string): void;',
 ].join('\n');
-
 const libUri = 'ts:filename/basic.d.ts';
 monaco.languages.typescript.javascriptDefaults.addExtraLib(libSource, libUri);
 monaco.editor.createModel(libSource, 'typescript', monaco.Uri.parse(libUri));
 for (const key of types.keys()) {
   const val = types(key).default;
   const uri = `ts:node/${key.split('./')[1]}`;
-  console.log(val, uri);
   monaco.languages.typescript.javascriptDefaults.addExtraLib(val, uri);
   monaco.editor.createModel(val, 'typescript', monaco.Uri.parse(uri));
 }
