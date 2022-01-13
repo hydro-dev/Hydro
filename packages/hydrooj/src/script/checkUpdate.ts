78fb88e4233656b1f8c7a1df69c894752bffa7c3
panda <panda_dtdyy@outlook.com>
2022-01-14 01:28:24 +0800

    SemVer -> semVer & editi and i18n verInfo text

--- packages/hydrooj/src/script/checkUpdate.ts
+++ packages/hydrooj/src/script/checkUpdate.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-await-in-loop */
-import * as SemVer from 'semver';
+import * as semVer from 'semver';
 import superagent from 'superagent';
 import MessageModel from '../model/message';
 import * as SystemModel from '../model/system';
@@ -23,15 +23,15 @@ export async function run() {
         const packNewVersion = await getpackNetVersion(name);
         if (!packNewVersion) continue;
         const packDBVersion = SystemModel.get(`checkVersion.${name}`);
-        if (SemVer.lt(packDBVersion, packNewVersion)) {
+        if (semVer.lt(packDBVersion, packNewVersion)) {
             SystemModel.set(`checkVersion.${name}`, packNewVersion);
-            if (SemVer.lt(packNowVersion[name], packNewVersion)) {
-                verInfo = `Package ${name}(v${packNowVersion[name]}) has a new version ${packNewVersion}, `;
+            if (semVer.lt(packNowVersion[name], packNewVersion)) {
+                verInfo += `\n${(name !== 'hydrooj') ? `@hydrooj/${name}` : name} v${packNowVersion[name]} -> ${packNewVersion}`;
             }
         }
     }
     if (!verInfo) {
-        MessageModel.sendNotification(`${verInfo}you should upgrade it for better experience.`);
+        MessageModel.sendNotification(`Packages have new version: ${verInfo}`);
     }
     return true;
 }
