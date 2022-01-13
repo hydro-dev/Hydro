1749b42a644f3f1adfb5bdc2a2a12f0f210b0268
panda <panda_dtdyy@outlook.com>
2022-01-14 00:45:34 +0800

    fix details

--- packages/hydrooj/src/script/checkUpdate.ts
+++ packages/hydrooj/src/script/checkUpdate.ts
@@ -1,29 +1,61 @@
 /* eslint-disable no-await-in-loop */
-import SuperAgent from 'superagent';
+import superagent from 'superagent';
 import { PRIV } from '../model/builtin';
+import MessageModel from '../model/message';
+import * as SystemModel from '../model/system';
+import UserModel from '../model/user';
 
 export const description = 'Get System Packages Latest Version From Yarn Registry';
 
+async function getpackNetVersion(packName:string) {
+    try {
+        const packInfoRes = await superagent.get(`https://registry.yarnpkg.com/${(packName !== 'hydrooj') ? `@hydrooj/${packName}` : packName}`);
+        return packInfoRes.body['dist-tags']['latest'];
+    } catch (e) {
+        return null;
+    }
+}
+
+function beyondVersion(versionA:string, versionB:string) {
+    const versionAarray = versionA.split('.');
+    const versionBarray = versionB.split('.');
+    const maxL = Math.max(versionAarray.length, versionBarray.length);
+    let result = 0;
+    for (let i = 0; i < maxL; i++) {
+        const verAValue = versionAarray.length > i ? parseInt(versionAarray[i], 10) : 0;
+        const verBValue = versionBarray.length > i ? parseInt(versionBarray[i], 10) : 0;
+        if (verAValue < verBValue) {
+            result = 1;
+            break;
+        } else if (verAValue > verBValue) {
+            result = -1;
+            break;
+        }
+    }
+    return result;
+}
+
 export async function run() {
-    const NowVersion = global.Hydro.version;
-    const Admins = global.Hydro.model.user.getMulti({ priv: { $bitsAllSet: PRIV.PRIV_VIEW_SYSTEM_NOTIFICATION } });
-    for (const pack in NowVersion) {
-        if (pack !== 'node') {
-            const packInfoRes = await SuperAgent.get(`https://registry.yarnpkg.com/${pack !== 'hydrooj' ? `@hydrooj/${pack}` : pack}`);
-            const packNewVersion = packInfoRes.body['dist-tags']['latest'];
-            const dbpackVersion = global.Hydro.model.system.get(`checkVersion.${pack}`);
-            if (dbpackVersion !== packNewVersion) {
-                global.Hydro.model.system.set(`checkVersion.${pack}`, packNewVersion);
-                if (NowVersion[pack] !== packNewVersion) {
-                    Admins.forEach(async (Udoc) => {
-                        await global.Hydro.model.message.send(1, Udoc._id,
-                            `Package ${pack}(v${NowVersion[pack]}) has a new version ${packNewVersion}, `
-                            + 'you should upgrade it for better experience.', 0);
-                    });
-                }
+    const packNowVersion = global.Hydro.version;
+    const admins = await UserModel.getMulti({ priv: { $bitsAllSet: PRIV.PRIV_VIEW_SYSTEM_NOTIFICATION } }).toArray();
+    for (const name in packNowVersion) {
+        if (name === 'node') continue;
+        const packNewVersion = await getpackNetVersion(name);
+        if (packNewVersion === null) continue;
+        const packDBVersion = SystemModel.get(`checkVersion.${name}`);
+        if (beyondVersion(packDBVersion, packNewVersion) === 1) {
+            SystemModel.set(`checkVersion.${name}`, packNewVersion);
+            if (beyondVersion(packNowVersion[name], packNewVersion) === 1) {
+                await Promise.all(
+                    admins.map((udoc) =>
+                        MessageModel.send(1, udoc._id,
+                            `Package ${name}(v${packNowVersion[name]}) has a new version ${packNewVersion}, `
+                            + 'you should upgrade it for better experience.', 0)),
+                );
             }
         }
     }
+    return true;
 }
 
 export const validate = {};
