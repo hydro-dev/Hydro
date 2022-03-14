3ebe126010976e19d6144d0b2c675e02b356db73
Henry-Chen0227 <2830093749@qq.com>
2022-03-14 19:33:51 +0800

    update

--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,22 @@
-cd root/Hydro
+if [ ! -d "/root/Hydro/" ]; then
+	echo "Creating..."
+	git clone git@github.com:Henry-Chen0227/Hydro.git /root/Hydro --recursive
+	cd /root/Hydro
+	yarn
+	yarn build:ui:production
+	echo "Created."
+else
+	LOCAL = $(git log)
+	REMOTE = $(git log origin/master)
+	if [ ! $LOCAL = $REMOTE ]; then
+		echo "Updating..."
+		cd /root/Hydro
+		git pull
+		yarn
+		yarn build:ui:production
+		echo "Updated."
+	fi
+fi
+cd /root/Hydro
 pm2 start "ulimit -s unlimited && sandbox" --name sandbox
 pm2-runtime start "yarn start --port=8888" --name hydrooj
