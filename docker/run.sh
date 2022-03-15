a740d61695ed699a09ed2a4b9250c84f8750be56
Henry-Chen0227 <2830093749@qq.com>
2022-03-15 14:09:11 +0800

    update

--- docker/run.sh
+++ docker/run.sh
@@ -2,18 +2,20 @@ if [ ! -d "/root/Hydro/" ];then
 	echo "Creating..."
 	git clone git@github.com:Henry-Chen0227/Hydro.git /root/Hydro --recursive
 	cd /root/Hydro
-	yarn
+	yarn install
 	yarn build:ui:production
 	echo "Created."
 else
 	cd /root/Hydro
 	LOCAL=$(git log)
 	REMOTE=$(git log origin/master)
-	if [ $LOCAL != $REMOTE ];then
+	if [ $LOCAL = $REMOTE ];then
+		echo "Updated."
+	else
 		echo "Updating..."
 		cd /root/Hydro
 		git pull
-		yarn
+		yarn install
 		yarn build:ui:production
 		echo "Updated."
 	fi
