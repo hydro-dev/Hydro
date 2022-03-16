fd3c519f203d2bb939884168d165540b9bfb0c11
panda <panda_dtdyy@outlook.com>
2022-03-17 00:44:55 +0800

    Add config route

--- packages/hydrooj/src/handler/problem.ts
+++ packages/hydrooj/src/handler/problem.ts
@@ -525,7 +525,8 @@ export class ProblemManageHandler extends ProblemDetailHandler {
 
 export class ProblemEditHandler extends ProblemManageHandler {
     async get() {
-        this.response.template = 'problem_edit.html';
+        this.response.body.additional_file = sortFiles(this.pdoc.additional_file || []);
+        this.response.template = 'problem_config.html';
     }
 
     @route('pid', Types.Name, null, parsePid)
@@ -547,6 +548,15 @@ export class ProblemEditHandler extends ProblemManageHandler {
         let pdoc = await problem.get(domainId, pid);
         pdoc = await problem.edit(domainId, pdoc.docId, $update);
         this.response.redirect = this.url('problem_detail', { pid: newPid || pdoc.docId });
+    }
+}
+
+export class ProblemConfigHandler extends ProblemManageHandler {
+    async get() {
+        this.response.body.testdata = sortFiles(this.pdoc.data || []);
+        const configFile = this.pdoc.data.filter(i => i.name === 'config.yaml');
+        this.response.body.config = configFile ? storage.get(`problem/${this.pdoc.domainId}/${this.pdoc.docId}/testdata/config.yaml`) : [];
+        this.response.template = 'problem_config.html';
     }
 }
 
@@ -831,6 +841,7 @@ export class ProblemCreateHandler extends Handler {
         this.response.template = 'problem_edit.html';
         this.response.body = {
             page_name: 'problem_create',
+            additional_file: [],
         };
     }
 
@@ -892,6 +903,7 @@ export async function apply() {
     Route('problem_detail', '/p/:pid', ProblemDetailHandler, PERM.PERM_VIEW_PROBLEM);
     Route('problem_submit', '/p/:pid/submit', ProblemSubmitHandler, PERM.PERM_SUBMIT_PROBLEM);
     Route('problem_edit', '/p/:pid/edit', ProblemEditHandler);
+    Route('problem_config', '/p/:pid/config', ProblemConfigHandler);
     Route('problem_files', '/p/:pid/files', ProblemFilesHandler, PERM.PERM_VIEW_PROBLEM);
     Route('problem_file_download', '/p/:pid/file/:filename', ProblemFileDownloadHandler, PERM.PERM_VIEW_PROBLEM);
     Route('problem_solution', '/p/:pid/solution', ProblemSolutionHandler, PERM.PERM_VIEW_PROBLEM);
