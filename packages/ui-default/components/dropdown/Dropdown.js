2a5c465c952fb6c8ada171a82bc37f5a180d7ead
panda <panda_dtdyy@outlook.com>
2022-03-21 11:14:08 +0800

    ui: add disabledconstrainToWindow to avoid filp

--- packages/ui-default/components/dropdown/Dropdown.js
+++ packages/ui-default/components/dropdown/Dropdown.js
@@ -30,7 +30,7 @@ export default class Dropdown extends DOMAttachedObject {
       content: this.options.target || $.find($dom.attr('data-dropdown-target'))[0],
       position: this.options.position,
       openOn: 'hover',
-      constrainToWindow: true,
+      constrainToWindow: $dom.attr('data-dropdown-disabledconstrainToWindow') === undefined,
       constrainToScrollParent: false,
     });
     this.dropInstance.on('open', this.onDropOpen.bind(this));
