209b31f6e685eb8fe04126a5cdfd5ccb5669d596
panda <panda_dtdyy@outlook.com>
2022-03-23 23:14:18 +0800

    fix nav-dropdown

--- packages/ui-default/components/dropdown/Dropdown.js
+++ packages/ui-default/components/dropdown/Dropdown.js
@@ -26,7 +26,7 @@ export default class Dropdown extends DOMAttachedObject {
     };
     this.dropInstance = new Drop({
       target: $dom[0],
-      classes: 'dropdown',
+      classes: `dropdown ${$dom.attr('data-dropdown-customClass') || ''}`,
       content: this.options.target || $.find($dom.attr('data-dropdown-target'))[0],
       position: this.options.position,
       openOn: 'hover',
