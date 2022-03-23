efe2c17f730a54b2fea778a4a3d4950899d8c677
panda <panda_dtdyy@outlook.com>
2022-03-24 00:10:24 +0800

    edit name

--- packages/ui-default/components/dropdown/Dropdown.js
+++ packages/ui-default/components/dropdown/Dropdown.js
@@ -26,7 +26,7 @@ export default class Dropdown extends DOMAttachedObject {
     };
     this.dropInstance = new Drop({
       target: $dom[0],
-      classes: `dropdown ${$dom.attr('data-dropdown-customClass') || ''}`,
+      classes: `dropdown ${$dom.attr('data-dropdown-custom-class') || ''}`,
       content: this.options.target || $.find($dom.attr('data-dropdown-target'))[0],
       position: this.options.position,
       openOn: 'hover',
