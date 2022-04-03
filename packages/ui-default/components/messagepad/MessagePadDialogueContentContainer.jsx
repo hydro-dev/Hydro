85e0b675ad411025aea9c9e25ffb8872d7fb2c92
Sonui <me@sonui.cn>
2022-04-03 11:55:17 +0800

    fix time show

--- packages/ui-default/components/messagepad/MessagePadDialogueContentContainer.jsx
+++ packages/ui-default/components/messagepad/MessagePadDialogueContentContainer.jsx
@@ -9,89 +9,92 @@ import { parse as parseMongoId } from 'vj/utils/mongoId';
 import Message from './MessageComponent';
 
 const mapStateToProps = (state) => ({
-    activeId: state.activeId,
-    item: state.activeId !== null
-        ? state.dialogues[state.activeId]
-        : null,
+  activeId: state.activeId,
+  item: state.activeId !== null
+    ? state.dialogues[state.activeId]
+    : null,
 });
 
 export default connect(mapStateToProps)(class MessagePadDialogueContentContainer extends React.PureComponent {
-    componentDidMount() {
-        $(this.refs.list).scrollLock({ strict: true });
+  componentDidMount() {
+    $(this.refs.list).scrollLock({ strict: true });
+  }
+
+  componentDidUpdate(prevProps) {
+    const node = this.refs.list;
+    if (this.props.activeId !== prevProps.activeId) {
+      this.scrollToBottom = true;
+      this.scrollWithAnimation = false;
+    } else if (node.scrollTop + node.offsetHeight === node.scrollHeight) {
+      this.scrollToBottom = true;
+      this.scrollWithAnimation = true;
+    } else this.scrollToBottom = false;
+
+    if (this.scrollToBottom) {
+      const targetScrollTop = node.scrollHeight - node.offsetHeight;
+      if (this.scrollWithAnimation) {
+        $(node).stop().animate({ scrollTop: targetScrollTop }, 200, 'easeOutCubic');
+      } else {
+        node.scrollTop = targetScrollTop;
+      }
     }
+  }
 
-    componentDidUpdate(prevProps) {
-        const node = this.refs.list;
-        if (this.props.activeId !== prevProps.activeId) {
-            this.scrollToBottom = true;
-            this.scrollWithAnimation = false;
-        } else if (node.scrollTop + node.offsetHeight === node.scrollHeight) {
-            this.scrollToBottom = true;
-            this.scrollWithAnimation = true;
-        } else this.scrollToBottom = false;
-
-        if (this.scrollToBottom) {
-            const targetScrollTop = node.scrollHeight - node.offsetHeight;
-            if (this.scrollWithAnimation) {
-                $(node).stop().animate({ scrollTop: targetScrollTop }, 200, 'easeOutCubic');
-            } else {
-                node.scrollTop = targetScrollTop;
-            }
+  renderContent(msg) {
+    // TODO: FLAG_RICHTEXT
+    if (msg.from === 1) {
+      // Is system message
+      try {
+        const data = JSON.parse(msg.content);
+        const str = i18n(data.message).replace(/\{([^{}]+)\}/g, (match, key) => `%substitude%${key}%substitude%`);
+        const arr = str.split('%substitude%');
+        data.params = data.params || {};
+        for (let i = 1; i < arr.length; i += 2) {
+          if (arr[i].endsWith(':link')) {
+            const link = data.params[arr[i].split(':link')[0]];
+            if (!link) continue;
+            arr[i] = <a style={{ color: 'wheat' }} href={link} key={i} target="_blank" rel="noreferrer">{link}</a>;
+          } else {
+            arr[i] = <span style={{ color: 'wheat' }} key={i}>{data.params[arr[i]]}</span>;
+          }
         }
+        return arr;
+      } catch (e) { }
+      return i18n(msg.content);
     }
+    return msg.content;
+  }
 
-    renderContent(msg) {
-        // TODO: FLAG_RICHTEXT
-        if (msg.from === 1) {
-            // Is system message
-            try {
-                const data = JSON.parse(msg.content);
-                const str = i18n(data.message).replace(/\{([^{}]+)\}/g, (match, key) => `%substitude%${key}%substitude%`);
-                const arr = str.split('%substitude%');
-                data.params = data.params || {};
-                for (let i = 1; i < arr.length; i += 2) {
-                    if (arr[i].endsWith(':link')) {
-                        const link = data.params[arr[i].split(':link')[0]];
-                        if (!link) continue;
-                        arr[i] = <a style={{ color: 'wheat' }} href={link} key={i} target="_blank" rel="noreferrer">{link}</a>;
-                    } else {
-                        arr[i] = <span style={{ color: 'wheat' }} key={i}>{data.params[arr[i]]}</span>;
-                    }
-                }
-                return arr;
-            } catch (e) { }
-            return i18n(msg.content);
+  renderInner() {
+    if (this.props.activeId === null) return [];
+    return this.props.item.messages.map((msg) => (
+      <Message
+        key={msg._id}
+        isSelf={msg.from === UserContext._id}
+        faceUrl={
+          msg.from === UserContext._id
+            ? UserContext.avatarUrl
+            : this.props.item.udoc.avatarUrl
         }
-        return msg.content;
-    }
+      >
+        <div>{this.renderContent(msg)}</div>
+        <div className="timeShow">
+          <time data-tooltip={moment(parseMongoId(msg._id).timestamp * 1000).format('YYYY-MM-DD HH:mm:ss')}>
+            <TimeAgo datetime={parseMongoId(msg._id).timestamp * 1000} locale={i18n('timeago_locale')} />
+          </time>
+        </div>
 
-    renderInner() {
-        if (this.props.activeId === null) return [];
-        return this.props.item.messages.map((msg) => (
-            <Message
-                key={msg._id}
-                isSelf={msg.from === UserContext._id}
-                faceUrl={
-                    msg.from === UserContext._id
-                        ? UserContext.avatarUrl
-                        : this.props.item.udoc.avatarUrl
-                }
-            >
-                <div>{this.renderContent(msg)}</div>
-                <time data-tooltip={moment(parseMongoId(msg._id).timestamp * 1000).format('YYYY-MM-DD HH:mm:ss')}>
-                    <TimeAgo datetime={parseMongoId(msg._id).timestamp * 1000} locale={i18n('timeago_locale')} />
-                </time>
-            </Message>
-        ));
-    }
+      </Message>
+    ));
+  }
 
-    render() {
-        return (
-            <div className="messagepad__content" ref="list">
-                <div className='lite-chatbox'>
-                    {this.renderInner()}
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="messagepad__content" ref="list">
+        <div className="lite-chatbox">
+          {this.renderInner()}
+        </div>
+      </div>
+    );
+  }
 });
