f87bddd4ce32089c9c4b8775a76cc51e07ba13fb
panda <panda_dtdyy@outlook.com>
2022-04-03 22:15:10 +0800

    ui: add problem_config

--- packages/ui-default/components/problemconfig/reducer/testdata.ts
+++ packages/ui-default/components/problemconfig/reducer/testdata.ts
@@ -1,51 +1,14 @@
-import _ from 'lodash';
-
-export default function reducer(state = {}, action) {
+export default function reducer(state = [], action) {
   switch (action.type) {
-  case 'DIALOGUES_LOAD_DIALOGUES_FULFILLED': {
-    const { messages } = action.payload;
-    return { ...state, ..._.keyBy(messages, 'udoc._id') };
+  case 'CONFIG_LOAD_FULFILLED': {
+    return state.concat(action.payload.testdata);
   }
-  case 'DIALOGUES_CREATE': {
-    const { user } = action.payload;
-    if (state[user._id]) return state;
-    return {
-      ...state,
-      [user._id]: {
-        _id: user._id,
-        udoc: { ...user },
-        messages: [],
-      },
-    };
+  case 'CONFIG_DELETE_TESTDATA': {
+    return state.filter((i) => i.name !== action.payload.name);
   }
-  case 'DIALOGUES_POST_SEND_FULFILLED': {
-    const id = action.meta.dialogueId;
-    const { mdoc } = action.payload;
-    return {
-      ...state,
-      [id]: {
-        ...state[id],
-        messages: [
-          ...state[id].messages,
-          mdoc,
-        ],
-      },
-    };
-  }
-  case 'DIALOGUES_MESSAGE_PUSH': {
-    const { mdoc, udoc } = action.payload;
-    const to = mdoc.from === UserContext._id ? mdoc.to : mdoc.from;
-    return {
-      ...state,
-      [to]: {
-        ...state[to] || {},
-        udoc,
-        messages: [
-          ...state[to]?.messages || [],
-          mdoc,
-        ],
-      },
-    };
+  case 'CONFIG_ADD_TESTDATA': {
+    state.push(action.payload);
+    return state;
   }
   default:
     return state;
