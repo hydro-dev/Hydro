42655d096c616a9bc3abcfa6237ee23f097c1826
panda <panda_dtdyy@outlook.com>
2022-04-11 11:19:41 +0800

    ui: fix testdata sidebar

--- packages/ui-default/components/problemconfig/reducer/testdata.ts
+++ packages/ui-default/components/problemconfig/reducer/testdata.ts
@@ -4,7 +4,7 @@ export default function reducer(state = [], action) {
     return state.concat(action.payload.testdata);
   }
   case 'CONFIG_DELETE_TESTDATA': {
-    const next = state.filter((i) => i.name !== action.payload.name);
+    const next = state.filter((i) => i.name !== action.value[0]);
     return next;
   }
   case 'CONFIG_ADD_TESTDATA': {
