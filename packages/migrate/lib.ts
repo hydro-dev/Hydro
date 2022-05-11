74f4e44f0fd714d16284c499028c377b651551b9
undefined <i@undefined.moe>
2022-05-11 21:01:58 +0800

    w: merge migration packages

--- packages/migrate/lib.ts
+++ packages/migrate/lib.ts
@@ -2,7 +2,7 @@ import { md5, sha1 } from 'hydrooj/src/lib/crypto';
 
 const RE_MD5 = /^[\da-f]{32}$/;
 
-function hash($password: string, $saved: string) {
+global.Hydro.lib['hash.hust'] = ($password: string, $saved: string) => {
     $password = md5($password);
     if (RE_MD5.test($saved)) return $password === $saved;
     const $svd = Buffer.from($saved, 'base64').toString('hex');
@@ -13,6 +13,11 @@ function hash($password: string, $saved: string) {
     ]).toString('base64');
     if ($hash.trim() === $saved.trim()) return true;
     return false;
-}
+};
 
-global.Hydro.lib['hash.hust'] = hash;
+global.Hydro.lib['hash.vj2'] = (password: string, salt: string, udoc: { uname: string; unameLower: string }) => {
+    const { uname, unameLower } = udoc;
+    const pmd5 = md5(password);
+    const mixedSha1 = sha1(md5(unameLower + pmd5) + salt + sha1(pmd5 + salt));
+    return `${Buffer.from(uname).toString('base64')}|${mixedSha1}`;
+};
