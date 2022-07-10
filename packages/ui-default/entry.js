2a54b0aa2551d474cc05c34ac87d1dee184d5bb5
undefined <i@undefined.moe>
2022-07-10 14:47:43 +0800

    ci: remove useless packtracker

--- packages/ui-default/entry.js
+++ packages/ui-default/entry.js
@@ -32,12 +32,6 @@ document.addEventListener('DOMContentLoaded', async () => {
   // eslint-disable-next-line camelcase
   try { __webpack_public_path__ = UiContext.cdn_prefix; } catch (e) { }
 
-  const res = await fetch(`/constant/${UiContext.constantVersion}`, { cache: 'force-cache' });
-  const data = await res.json();
-  eval(data[0]); // eslint-disable-line no-eval
-  data.shift();
-  window.Hydro.preload = data;
-
   const e = document.createElement('style');
   const dark = UiContext.domain.nav_logo_dark || UiContext.nav_logo_dark;
   const dark2x = UiContext.domain.nav_logo_dark_2x || UiContext.nav_logo_dark_2x;
@@ -53,6 +47,12 @@ document.addEventListener('DOMContentLoaded', async () => {
         }
       }` : ''}`;
   document.body.appendChild(e);
+
+  const res = await fetch(`/constant/${UiContext.constantVersion}`, { cache: 'force-cache' });
+  const data = await res.json();
+  eval(data[0]); // eslint-disable-line no-eval
+  data.shift();
+  window.Hydro.preload = data;
 
   import('./hydro');
 }, false);
