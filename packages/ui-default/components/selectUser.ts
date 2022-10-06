0facd0b43315a9796a4cda6c3d76007893df10a7
undefined <i@undefined.moe>
2022-10-06 20:15:39 +0800

    ui: add selectUser api

---
+++ packages/ui-default/components/selectUser.ts
@@ -0,0 +1,44 @@
+import UserSelectAutoComplete from 'vj/components/autocomplete/UserSelectAutoComplete';
+import { ActionDialog } from 'vj/components/dialog';
+import i18n from 'vj/utils/i18n';
+import tpl from 'vj/utils/tpl';
+import createHint from './hint';
+
+let inserted = false;
+
+export default async function selectUser(hint?: string) {
+  if (!inserted) {
+    const el = $(tpl`<div style="display: none" class="dialog__body--user-select">
+      <div class="row"><div class="columns">
+        <h1 name="select_user_hint">${i18n('Select User')}</h1>
+      </div></div>
+      <div class="row">
+        <div class="columns">
+          <label>${i18n('Username / UID')}
+            <input name="user" type="text" class="textbox" autocomplete="off" data-autofocus>
+          </label>
+        </div>
+      </div>
+    </div>`);
+    el.appendTo(document.body);
+    if (hint) createHint(hint, $('[name="select_user_hint"]'));
+    inserted = true;
+  }
+  const userSelector = UserSelectAutoComplete.getOrConstruct($('.dialog__body--user-select [name="user"]')) as any;
+  const userSelectDialog = new ActionDialog({
+    $body: $('.dialog__body--user-select > div'),
+    onDispatch(action) {
+      if (action === 'ok' && userSelector.value() === null) {
+        userSelector.focus();
+        return false;
+      }
+      return true;
+    },
+  });
+  userSelector.clear();
+  const action = await userSelectDialog.open();
+  if (action !== 'ok') return null;
+  return userSelector.value();
+}
+
+window.Hydro.components.selectUser = selectUser;
