32d3d700038c757c5c4088096521ad4ab3eba9c1
undefined <i@undefined.moe>
2022-10-06 21:11:51 +0800

    ignore default user

--- packages/hydrooj/src/handler/manage.ts
+++ packages/hydrooj/src/handler/manage.ts
@@ -272,8 +272,8 @@ class SystemUserPrivHandler extends SystemHandler {
     @requireSudo
     async get({ pjax }) {
         const defaultPriv = system.get('default.priv');
-        const udocs = await user.getMulti({ _id: { $gte: -1000 }, priv: { $nin: [0, defaultPriv] } }).limit(1000).sort({ _id: 1 }).toArray();
-        const banudocs = await user.getMulti({ _id: { $gte: -1000 }, priv: 0 }).limit(1000).sort({ _id: 1 }).toArray();
+        const udocs = await user.getMulti({ _id: { $gte: -1000, $ne: 1 }, priv: { $nin: [0, defaultPriv] } }).limit(1000).sort({ _id: 1 }).toArray();
+        const banudocs = await user.getMulti({ _id: { $gte: -1000, $ne: 1 }, priv: 0 }).limit(1000).sort({ _id: 1 }).toArray();
         this.response.body = {
             udocs: [...udocs, ...banudocs],
             defaultPriv,
