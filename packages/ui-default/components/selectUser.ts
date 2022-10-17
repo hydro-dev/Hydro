8c54a2354591c4b10d4899a010de6d1723e1cd98
panda <panda_dtdyy@outlook.com>
2022-10-18 02:14:28 +0800

    ui: fix selectUser dialog

--- packages/ui-default/components/selectUser.ts
+++ packages/ui-default/components/selectUser.ts
@@ -8,22 +8,24 @@ let inserted = false;
 
 export default async function selectUser(hint?: string) {
   if (!inserted) {
-    const el = $(tpl`<div style="display: none" class="dialog__body--user-select">
-      <div class="row"><div class="columns">
-        <h1 name="select_user_hint">${i18n('Select User')}</h1>
-      </div></div>
-      <div class="row">
-        <div class="columns">
-          <label>${i18n('Username / UID')}
-            <input name="user" type="text" class="textbox" autocomplete="off" data-autofocus>
-          </label>
-        </div>
-      </div>
-    </div>`);
-    el.appendTo(document.body);
-    if (hint) createHint(hint, $('[name="select_user_hint"]'));
+    const dialog = $(tpl`<div style="display: none" class="dialog__body--user-select"></div>`);
+    dialog.appendTo(document.body);
     inserted = true;
   }
+  const el = $(tpl`
+    <div class="row"><div class="columns">
+      <h1 name="select_user_hint">${i18n('Select User')}</h1>
+    </div></div>
+    <div class="row">
+      <div class="columns">
+        <label>${i18n('Username / UID')}
+          <input name="user" type="text" class="textbox" autocomplete="off" data-autofocus>
+        </label>
+      </div>
+    </div>
+  </div>`);
+  el.appendTo($('.dialog__body--user-select'));
+  if (hint) createHint(hint, $('[name="select_user_hint"]'));
   const userSelector = UserSelectAutoComplete.getOrConstruct($('.dialog__body--user-select [name="user"]')) as any;
   const userSelectDialog = new ActionDialog({
     $body: $('.dialog__body--user-select > div'),
