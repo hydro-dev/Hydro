14322096487dc12abeb25770d77c82d43c62c018
undefined <i@undefined.moe>
2022-10-19 09:35:50 +0800

    ui: sw: precache from different origin

--- packages/ui-default/service-worker.ts
+++ packages/ui-default/service-worker.ts
@@ -19,6 +19,7 @@ self.addEventListener('notificationclick', (event) => {
 });
 
 const PRECACHE = `precache-${process.env.VERSION}`;
+const DO_NOT_CACHE = ['vditor', '.worker.js', 'fonts', 'i.monaco'];
 
 function shouldCache(name: string) {
   if (!name.split('/').pop()) return false;
@@ -27,73 +28,76 @@ function shouldCache(name: string) {
 }
 function shouldPreCache(name: string) {
   if (!shouldCache(name)) return false;
-  if (['vditor', '.worker.js', 'fonts', 'i.monaco'].filter((i) => name.includes(i))) return false;
+  if (DO_NOT_CACHE.filter((i) => name.includes(i)).length) return false;
   return true;
 }
 
 interface ServiceWorkerConfig {
   base?: string;
   targets?: string[];
-  preload?: boolean;
+  preload?: string;
 }
-const config: ServiceWorkerConfig = null;
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    Promise.all([
-      caches.open(PRECACHE),
-      fetch('/manifest.json').then((res) => res.json()),
-    ]).then(([cache, manifest]) => {
-      if (process.env.NODE_ENV !== 'production' || !config?.preload) return;
-      // TODO fetch cache from different origin
-      cache.addAll(Object.values(manifest).filter(shouldPreCache) as string[]);
-    }).then(() => self.skipWaiting()),
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  const currentCaches = [PRECACHE];
-  event.waitUntil(
-    caches.keys()
-      .then((cacheNames) => cacheNames.filter((cacheName) => !currentCaches.includes(cacheName)))
-      .then((cachesToDelete) => Promise.all(cachesToDelete.map((cacheToDelete) => caches.delete(cacheToDelete))))
-      .then(() => self.clients.claim()),
-  );
-});
+let config: ServiceWorkerConfig = null;
 
 async function get(url: string) {
-  let current = 0;
+  const pending = [url, ...(config?.targets || []).map((i) => url.replace(config.base, i))];
   let response: Promise<Response>;
-  while (config.targets[current]) {
-    const source = config.targets[current];
-    response = fetch(url.replace(config.base, source));
+  for (const source of pending) {
+    response = fetch(source);
     try {
-      await response;
-      if (current) console.log('From ', source);
-      return response;
+      console.log('From ', source);
+      const r = await response;
+      if (r.ok) return r;
     } catch (error) {
       console.warn(source, ' Load fail ', error);
-      current++;
     }
   }
   return response;
 }
 
+self.addEventListener('install', (event) => event.waitUntil((async () => {
+  const [cache, manifest, cfg] = await Promise.all([
+    caches.open(PRECACHE),
+    fetch('/manifest.json').then((res) => res.json()),
+    fetch('/sw-config').then((res) => res.json()),
+  ]);
+  config = cfg;
+  if (process.env.NODE_ENV === 'production' && config?.preload) {
+    const files = Object.values(manifest).filter(shouldPreCache)
+      .map((i: string) => new URL(i, config.preload).toString());
+    await cache.addAll(files); // NOTE: CORS header
+  }
+  self.skipWaiting();
+})()));
+
+self.addEventListener('activate', (event) => {
+  const valid = [PRECACHE];
+  event.waitUntil(
+    caches.keys()
+      .then((names) => Promise.all(
+        names.filter((name) => !valid.includes(name)).map((p) => caches.delete(p)),
+      )).then(() => self.clients.claim()),
+  );
+});
+
 self.addEventListener('fetch', (event: FetchEvent) => {
   if (!['get', 'head'].includes(event.request.method.toLowerCase())) return;
+  if (!event.request.url.startsWith('http')) return;
   if (!shouldCache(event.request.url) && config?.base && config?.targets?.length) {
     if (new URL(event.request.url).origin !== location.origin) return;
     event.respondWith(get(event.request.url));
+    return;
   }
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) return cachedResponse;
-      if (process.env.NODE_ENV !== 'production') return fetch(event.request);
-      console.log(`Caching ${event.request.url}`);
-      return caches.open(PRECACHE)
-        .then((cache) => fetch(event.request)
-          .then((response) => cache.put(event.request, response.clone())
-            .then(() => response)));
-    }),
-  );
+  event.respondWith((async () => {
+    const cachedResponse = await caches.match(event.request);
+    if (cachedResponse) return cachedResponse;
+    if (process.env.NODE_ENV !== 'production') return fetch(event.request);
+    console.log(`Caching ${event.request.url}`);
+    const [cache, response] = await Promise.all([
+      caches.open(PRECACHE),
+      fetch(event.request),
+    ]);
+    if (response.ok) await cache.put(event.request, response.clone());
+    return response;
+  })());
 });
