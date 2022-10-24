701598fda4d374f923a600b41802eadca5444be8
panda <panda_dtdyy@outlook.com>
2022-10-24 23:04:36 +0800

    ui: fix contest progress

--- packages/ui-default/pages/contest.page.ts
+++ packages/ui-default/pages/contest.page.ts
@@ -10,6 +10,7 @@ export default new NamedPage(['contest_detail', 'contest_detail_problem', 'conte
   const beginAt = new Date(UiContext.tdoc.beginAt).getTime();
   const endAt = new Date(UiContext.tdoc.endAt).getTime();
   NProgress.configure({ trickle: false, showSpinner: false, minimum: 0 });
+  let progressTimer;
   function updateProgress() {
     const now = Date.now();
     if (beginAt <= now && now <= endAt) {
@@ -20,9 +21,10 @@ export default new NamedPage(['contest_detail', 'contest_detail_problem', 'conte
       if (now > endAt) NProgress.set(1);
       else NProgress.set(0);
       contestTimer.hide();
+      clearInterval(progressTimer);
     }
   }
   NProgress.start();
   updateProgress();
-  setInterval(updateProgress, 1000);
+  progressTimer = setInterval(updateProgress, 1000);
 });
