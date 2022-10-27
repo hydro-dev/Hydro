18e077a199f1029e26fef83fb30eb26f53f5264c
undefined <i@undefined.moe>
2022-10-27 13:26:21 +0800

    ui: update history render

--- packages/ui-default/components/discussion/history.page.tsx
+++ packages/ui-default/components/discussion/history.page.tsx
@@ -1,6 +1,7 @@
 import { Popover } from '@blueprintjs/core';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
 import TimeAgo from 'timeago-react';
 import { InfoDialog } from 'vj/components/dialog';
 import { AutoloadPage } from 'vj/misc/Page';
@@ -9,35 +10,42 @@ import request from 'vj/utils/request';
 import tpl from 'vj/utils/tpl';
 
 async function historyDialog(payload, time, uid) {
-  const rawHtml = await fetch(`${payload}?time=${new Date(time).getTime()}`).then((res) => res.text());
+  const ts = new Date(time).getTime();
+  const rawHtml = await fetch(`${payload}?time=${ts}&render=1`).then((res) => res.text());
   new InfoDialog({
     $body: tpl`
       <div class="typo">
-        <p><div data-user>${uid}</div> ${i18n('Edited at')} <span class="time relative" data-timestamp="${time / 1000}">${time / 1000}</span></p>
+        <p><div data-user>${uid}</div> ${i18n('Edited at')} <span class="time relative" data-timestamp="${ts / 1000}">${time}</span></p>
         ${{ templateRaw: true, html: rawHtml }}
       </div>`,
   }).open();
 }
 
+const queryClient = new QueryClient();
+
 function History({ payload }) {
-  const [history, updateHistory] = React.useState([]);
-  const [loading, updateLoading] = React.useState(true);
-  if (loading) {
-    try {
-      request.get(`${payload}?all=1`).then((res) => updateHistory(res.history), updateLoading(false));
-    } catch (e) { updateLoading(false); }
-  }
+  const [load, setLoad] = React.useState(false);
+  const { isLoading, isError, data } = useQuery(['history', payload], async () => {
+    const { history } = await request.get(`${payload}?all=1`);
+    return history;
+  }, { enabled: !!load });
   return (
-    <Popover usePortal interactionKind="hover">
-      <a>{ i18n('Edited') }</a>
+    <Popover usePortal interactionKind="hover" position="bottom" onOpening={() => setLoad(true)}>
+      <span>{i18n('Edited')} <span className="icon icon-expand_more"></span></span>
       <ol className="menu">
-        {!history.length && <li className="menu__item">{ loading ? 'Loading...' : 'Loading failed.' }</li>}
-        {history.map((item) => (
+        {(isLoading || isError) && (
+          <li className="menu__item">
+            <a className="menu__link">
+              {isLoading ? i18n('Loading...') : i18n('Loading failed.')}
+            </a>
+          </li>
+        )}
+        {data?.length && data.map((item) => (
           <li className="menu__item" key={item.time}>
             <a className="menu__link" onClick={() => historyDialog(payload, item.time, item.uid)}>
               {i18n('Edited at')}
               {' '}
-              <time><TimeAgo datetime={item.time} locale={i18n('timeago_locale')} /></time>
+              <TimeAgo datetime={item.time} locale={i18n('timeago_locale')} />
             </a>
           </li>
         ))}
@@ -47,17 +55,15 @@ function History({ payload }) {
 }
 
 const page = new AutoloadPage('discussionHistoryPage', () => {
-  if ($('[data-discussion-history]').length) {
-    const rendered = [];
-    $(document).on('mouseover', '[data-discussion-history]', (ev) => {
-      const $el = $(ev.currentTarget);
-      if (rendered.includes($el.data('raw-url'))) return;
-      ReactDOM.createRoot($el.get(0)).render(
-        <History payload={$el.data('raw-url')} />,
-      );
-      rendered.push($el.data('raw-url'));
-    });
-  }
+  $('[data-discussion-history]').each((i, e) => {
+    const url = $(e).data('raw-url');
+    if (!url) return;
+    ReactDOM.createRoot(e).render(
+      <QueryClientProvider client={queryClient}>
+        <History payload={url} />
+      </QueryClientProvider>,
+    );
+  });
 });
 
 export default page;
