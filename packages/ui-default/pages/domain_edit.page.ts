da12c88b498650483bea683413218dae046a4e52
panda <panda_dtdyy@outlook.com>
2022-10-31 00:43:23 +0800

    fix i18n

--- packages/ui-default/pages/domain_edit.page.ts
+++ packages/ui-default/pages/domain_edit.page.ts
@@ -1,9 +1,10 @@
 import CustomSelectAutoComplete from 'vj/components/autocomplete/CustomSelectAutoComplete';
 import { NamedPage } from 'vj/misc/Page';
+import i18n from 'vj/utils/i18n';
 
 const page = new NamedPage('domain_edit', () => {
   const langs = [
-    { key: '_', name: '客观题' },
+    { key: '_', name: i18n('objective') },
     ...Object.keys(window.LANGS).map((i) => ({ key: i, name: window.LANGS[i].display })),
   ];
   CustomSelectAutoComplete.getOrConstruct($('[name="langs"]'), { multi: true, data: langs });
