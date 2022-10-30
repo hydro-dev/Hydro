fb7d2627cc3e61c35ee434a85477c19aac05378a
panda <panda_dtdyy@outlook.com>
2022-10-31 00:47:06 +0800

    do not show langs prefix

--- packages/ui-default/pages/domain_edit.page.ts
+++ packages/ui-default/pages/domain_edit.page.ts
@@ -3,9 +3,10 @@ import { NamedPage } from 'vj/misc/Page';
 import i18n from 'vj/utils/i18n';
 
 const page = new NamedPage('domain_edit', () => {
+  const prefixes = new Set(Object.keys(window.LANGS).filter((i) => i.includes('.')).map((i) => i.split('.')[0]));
   const langs = [
     { key: '_', name: i18n('objective') },
-    ...Object.keys(window.LANGS).map((i) => ({ key: i, name: window.LANGS[i].display })),
+    ...Object.keys(window.LANGS).filter((i) => !prefixes.has(i)).map((i) => ({ name: window.LANGS[i].display, _id: i })),
   ];
   CustomSelectAutoComplete.getOrConstruct($('[name="langs"]'), { multi: true, data: langs });
 });
