e827f26358e85c74ba2265b23bec3baad198849b
panda <panda_dtdyy@outlook.com>
2022-11-01 00:52:03 +0800

    add prefix display & show all langs

--- packages/ui-default/pages/domain_edit.page.ts
+++ packages/ui-default/pages/domain_edit.page.ts
@@ -3,12 +3,13 @@ import { NamedPage } from 'vj/misc/Page';
 import i18n from 'vj/utils/i18n';
 
 const page = new NamedPage('domain_edit', () => {
-  const prefixes = new Set(Object.keys(window.LANGS).filter((i) => i.includes('.')).map((i) => i.split('.')[0]));
   const langs = [
     { key: '_', name: i18n('objective') },
-    ...Object.keys(window.LANGS).filter((i) => !prefixes.has(i)).map((i) => ({ name: window.LANGS[i].display, _id: i })),
+    ...Object.keys(window.LANGS).map((i) => (
+      { name: `${i.includes('.') ? `${window.LANGS[i.split('.')[0]].display}/` : ''}${window.LANGS[i].display}`, _id: i }
+    )),
   ];
-  CustomSelectAutoComplete.getOrConstruct($('[name="langs"]'), { multi: true, data: langs });
+  CustomSelectAutoComplete.getOrConstruct($('[name=langs]'), { multi: true, data: langs });
 });
 
 export default page;
