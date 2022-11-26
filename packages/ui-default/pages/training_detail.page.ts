439e8d935f071d4e13907eeba96dacb80f3deb25
panda <panda_dtdyy@outlook.com>
2022-11-26 15:54:49 +0000

    auto expand current section

--- packages/ui-default/pages/training_detail.page.ts
+++ packages/ui-default/pages/training_detail.page.ts
@@ -4,19 +4,25 @@ import { NamedPage } from 'vj/misc/Page';
 import pjax from 'vj/utils/pjax';
 import { slideDown, slideUp } from 'vj/utils/slide';
 
-async function handleSection(ev, type: string) {
-  const $section = $(ev.currentTarget).closest('.training__section');
-  if ($section.is(`.${type}d, .animating`)) return;
+type SectionState = 'expanded' | 'collapsed';
+
+async function setSectionState($section: JQuery<HTMLElement>, state: SectionState) {
+  if ($section.is(`.${state}, .animating`)) return;
   $section.addClass('animating');
   const $detail = $section.find('.training__section__detail');
-  if (type === 'expand') {
+  if (state === 'expanded') {
     await slideDown($detail, 300, { opacity: 0 }, { opacity: 1 });
   } else {
     await slideUp($detail, 300, { opacity: 1 }, { opacity: 0 });
   }
-  $section.addClass(type === 'expand' ? 'expanded' : 'collapsed');
-  $section.removeClass(type === 'expand' ? 'collapsed' : 'expanded');
+  $section.addClass(state);
+  $section.removeClass(state === 'expanded' ? 'collapsed' : 'expanded');
   $section.removeClass('animating');
+}
+
+async function handleSection(ev: JQuery.ClickEvent<Document>, type: SectionState) {
+  const $section = $(ev.currentTarget).closest('.training__section');
+  await setSectionState($section, type);
 }
 
 function searchUser() {
@@ -43,12 +49,29 @@ function handleChooseUser(ev) {
   pjax.request({ url: ev.currentTarget.href });
 }
 
+async function handleSidebarClick(ev: JQuery.ClickEvent<Document>) {
+  const id = $(ev.currentTarget).attr('href');
+  const $section = $(id).closest('.training__section');
+  await setSectionState($section, 'expanded');
+}
+
+async function handleHashChange() {
+  const id = window.location.hash;
+  if (id.startsWith('#node-')) {
+    const $section = $(id).closest('.training__section');
+    await setSectionState($section, 'expanded');
+  }
+}
+
 const page = new NamedPage('training_detail', () => {
   $('.search__input').on('input', _.debounce(searchUser, 500));
   $('#searchForm').on('submit', selectUser);
-  $(document).on('click', '[name="training__section__expand"]', (ev) => handleSection(ev, 'expand'));
-  $(document).on('click', '[name="training__section__collapse"]', (ev) => handleSection(ev, 'collapse'));
+  $(document).on('click', '[name="training__section__expand"]', (ev) => handleSection(ev, 'expanded'));
+  $(document).on('click', '[name="training__section__collapse"]', (ev) => handleSection(ev, 'collapsed'));
   $(document).on('click', '.enroll_user_menu_item > a', (ev) => handleChooseUser(ev));
+  $(document).on('click', '#menu-item-training_detail > ul > li > a', (ev) => handleSidebarClick(ev));
+  window.addEventListener('hashchange', handleHashChange);
+  $(handleHashChange);
 });
 
 export default page;
