fdb73f4d0f671abb7d0f7f98ccdf5c7092e95924
undefined <i@undefined.moe>
2023-03-13 01:22:47 +0800

    remove unused import

--- packages/ui-default/components/problemconfig/index.tsx
+++ packages/ui-default/components/problemconfig/index.tsx
@@ -1,5 +1,4 @@
-import { Tab, Tabs } from '@blueprintjs/core';
-import React, { useState } from 'react';
+import React from 'react';
 import { i18n } from 'vj/utils';
 import ProblemConfigEditor from './ProblemConfigEditor';
 import ProblemConfigForm from './ProblemConfigForm';
@@ -10,8 +9,6 @@ interface Props {
 }
 
 export default function ProblemConfig(props: Props) {
-  const [currentSelection, setCurrentSelection] = useState(null);
-
   return (<>
     <div className="row">
       <div className="medium-5 columns">
