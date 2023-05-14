3c39786284d485bd3bf31ff2f288e37d59050c04
panda <panda_dtdyy@outlook.com>
2023-05-14 15:19:48 +0000

    fix tsdoc on scoreboard

--- packages/hydrooj/src/handler/contest.ts
+++ packages/hydrooj/src/handler/contest.ts
@@ -291,8 +291,9 @@ export class ContestScoreboardHandler extends ContestDetailBaseHandler {
         const page_name = this.tdoc.rule === 'homework'
             ? 'homework_scoreboard'
             : 'contest_scoreboard';
+        const tsdoc = pick(this.tsdoc, ['attend', 'startAt', ...(this.tdoc.duration ? ['endAt'] : [])]);
         this.response.body = {
-            tdoc: this.tdoc, tsdoc: this.tsdoc, rows, udict, pdict, page_name, groups,
+            tdoc: this.tdoc, tsdoc, rows, udict, pdict, page_name, groups,
         };
         this.response.pjax = 'partials/scoreboard.html';
         this.response.template = 'contest_scoreboard.html';
@@ -616,7 +617,7 @@ export class ContestFileDownloadHandler extends ContestDetailBaseHandler {
 export class ContestUserHandler extends ContestManagementBaseHandler {
     @param('tid', Types.ObjectId)
     async get(domainId: string, tid: ObjectId) {
-        const tsdocs = await contest.getMultiStatus(domainId, { docId: tid }).toArray();
+        const tsdocs = await contest.getMultiStatus(domainId, { docId: tid }).project({ uid: 1, attend: 1, startAt: 1 }).toArray();
         tsdocs.forEach((i) => {
             i.endAt = (this.tdoc.duration && i.startAt) ? moment(i.startAt).add(this.tdoc.duration, 'hours').toDate() : null;
         });
