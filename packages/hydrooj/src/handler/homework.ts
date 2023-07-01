7a3d7cd42ae8a88efb56c85b56c4840c7cfd48a0
panda <panda_dtdyy@outlook.com>
2023-07-01 15:33:34 +0000

    core: allow manager filter all groups

--- packages/hydrooj/src/handler/homework.ts
+++ packages/hydrooj/src/handler/homework.ts
@@ -24,7 +24,8 @@ class HomeworkMainHandler extends Handler {
     @param('group', Types.Name, true)
     @param('page', Types.PositiveInt, true)
     async get(domainId: string, group = '', page = 1) {
-        const groups = (await user.listGroup(domainId, this.user._id)).map((i) => i.name).filter((i) => !Number.isSafeInteger(+i));
+        const groups = (await user.listGroup(domainId, this.user.hasPerm(PERM.PERM_VIEW_HIDDEN_CONTEST) ? undefined : this.user._id))
+            .map((i) => i.name).filter((i) => !Number.isSafeInteger(+i));
         if (group && !groups.includes(group)) throw new NotAssignedError(group);
         const cursor = contest.getMulti(domainId, {
             rule: 'homework',
