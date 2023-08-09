2bea4624d772162ff69a4949cdc1c7d8f630f220
LYkcul <126674875+LYkcul@users.noreply.github.com>
2023-08-09 15:48:58 +0800

    core: fix permission check for deleting discussion replies

--- packages/hydrooj/src/handler/discussion.ts
+++ packages/hydrooj/src/handler/discussion.ts
@@ -321,14 +321,14 @@ class DiscussionDetailHandler extends DiscussionHandler {
     @param('drrid', Types.ObjectId)
     async postDeleteTailReply(domainId: string, drid: ObjectId, drrid: ObjectId) {
         const deleteBy = this.user.own(this.drrdoc) ? 'self' : 'Admin';
-        if (!(this.user.own(this.ddoc)
-        && this.user.hasPerm(PERM.PERM_DELETE_DISCUSSION_REPLY_SELF_DISCUSSION))) {
-            if (this.user.own(this.drrdoc)) {
-                this.checkPerm(PERM.PERM_DELETE_DISCUSSION_REPLY_SELF);
-            } else {
+        if (!(this.user.own(this.drrdoc)
+        && this.user.hasPerm(PERM.PERM_DELETE_DISCUSSION_REPLY_SELF))) {
+            if (!this.user.own(this.drrdoc)) {
                 this.checkPerm(PERM.PERM_DELETE_DISCUSSION_REPLY);
+            } else {
+                this.checkPerm(PERM.PERM_DELETE_DISCUSSION_REPLY_SELF);
             }
-    }
+        }
         const msg = JSON.stringify({
             message: 'Admin {0} delete your discussion tail reply {1} in "{2}"({3:link}).',
             params: [
