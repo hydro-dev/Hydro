b73afc7af6b46e11dab62526611ba8444720a877
panda <panda_dtdyy@outlook.com>
2023-08-17 06:39:35 +0800

    core: fix owner display on problem & contest (#614)

--- packages/hydrooj/src/handler/problem.ts
+++ packages/hydrooj/src/handler/problem.ts
@@ -376,6 +376,7 @@ export class ProblemDetailHandler extends ContestDetailBaseHandler {
             solutionCount: scnt,
             discussionCount: dcnt,
             tdoc: this.tdoc,
+            owner_udoc: (tid && this.tdoc.owner !== this.pdoc.owner) ? await user.getById(domainId, this.tdoc.owner) : null,
         };
         if (this.tdoc && this.tsdoc) {
             const fields = ['attend', 'startAt'];
