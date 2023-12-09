dac545a084f3e51e8c16aa71703e43b9fec03ff8
LYkcul <pandaliu0812@163.com>
2023-12-09 11:30:39 +0800

    judge: fix unknown checker_type

--- packages/hydrojudge/src/judge/interactive.ts
+++ packages/hydrojudge/src/judge/interactive.ts
@@ -68,7 +68,7 @@ export const judge = async (ctx: Context) => await runFlow(ctx, {
     compile: async () => {
         [ctx.executeUser, ctx.executeInteractor] = await Promise.all([
             ctx.compile(ctx.lang, ctx.code),
-            ctx.compileWithTestlib('interactor', ctx.config.interactor),
+            ctx.compileWithTestlib('interactor', ctx.config.interactor, ctx.config.checker_type),
         ]);
     },
     judgeCase,
