f1f122e8034ed5a005628550f17a21461d6487ee
air-adteam <2399824990@qq.com>
2024-01-26 01:37:03 +0000

    set platform is optional actions

--- packages/hydrooj/src/model/oauth.ts
+++ packages/hydrooj/src/model/oauth.ts
@@ -2,19 +2,19 @@ import db from '../service/db';
 
 const coll = db.collection('oauth');
 
+/**
+    This is for compatibility with old Hydro OAuth modules
+    Old versions of Hydro may not save platform names
+    It is recommended that all plugin writers register platform information, as compatibility is reserved here.
+*/
 class OauthModel {
-    static async get(_id: string, platform: string) {
+    static async get(_id: string, platform?: string) {
         const doc = await coll.findOne({ _id });
-        /**
-        This is for compatibility with old Hydro OAuth modules
-        Old versions of Hydro may not save platform names
-        It is recommended that all plugin writers register platform information, as compatibility is reserved here.
-        */
         if (doc && (doc.platform === platform || !doc.platform)) return doc.uid;
         return null;
     }
 
-    static async set(_id: string, uid: number, platform: string) {
+    static async set(_id: string, uid: number, platform?: string) {
         const res = await coll.findOneAndUpdate(
             { _id },
             { $set: { uid, platform } },
