e9a1ece808aecf40b73d63beab8bdf8c91e6ac8e
Yang Gao <6821729+criyle@users.noreply.github.com>
2024-04-09 08:13:50 +0800

    core: async hash calculation (#779)

--- packages/hydrooj/src/lib/hash.hydro.ts
+++ packages/hydrooj/src/lib/hash.hydro.ts
@@ -1,7 +1,12 @@
 import crypto from 'crypto';
 
-function hash(password: string, salt: string) {
-    return crypto.pbkdf2Sync(password, salt, 100000, 64, 'sha256').toString('hex').substring(0, 64);
+function hash(password: string, salt: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+        crypto.pbkdf2(password, salt, 100000, 64, 'sha256', (err, key) => {
+            if (err) reject(err);
+            else resolve(key.toString('hex').substring(0, 64));
+        });
+    });
 }
 
 export = hash;
