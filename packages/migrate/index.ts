510d156383df735d1025398333599180593fbca0
panda <panda_dtdyy@outlook.com>
2024-05-10 15:46:47 +0800

    migrate: add poj & fix bug (#801)

--- packages/migrate/index.ts
+++ packages/migrate/index.ts
@@ -9,13 +9,13 @@ export function apply(ctx: Context) {
     ctx.addScript(
         'migrateHustoj', 'migrate from hustoj',
         Schema.object({
-            host: Schema.string().required(),
-            port: Schema.number().required(),
-            name: Schema.string().required(),
+            host: Schema.string().default('localhost'),
+            port: Schema.number().default(3306),
+            name: Schema.string().default('jol'),
             username: Schema.string().required(),
             password: Schema.string().required(),
-            domainId: Schema.string().required(),
-            contestType: Schema.string().required(),
+            domainId: Schema.string().default('system'),
+            contestType: Schema.string().default('oi'),
             dataDir: Schema.string().required(),
             uploadDir: Schema.string().default('/home/judge/src/web/upload/'),
         }),
@@ -46,7 +46,7 @@ export function apply(ctx: Context) {
         (...args) => require('./scripts/vijos').run(...args),
     );
     ctx.addScript(
-        'migrateuniversaloj', 'migrate from universaloj',
+        'migrateUniversaloj', 'migrate from universaloj',
         Schema.object({
             host: Schema.string().default('172.17.0.2'),
             port: Schema.number().default(3306),
@@ -57,6 +57,21 @@ export function apply(ctx: Context) {
             dataDir: Schema.string().required(),
         }),
         (...args) => require('./scripts/universaloj').run(...args),
+    );
+    ctx.addScript(
+        'migratePoj', 'migrate from poj',
+        Schema.object({
+            host: Schema.string().required(),
+            port: Schema.number().default(3306),
+            name: Schema.string().default('judgeonline'),
+            username: Schema.string().required(),
+            password: Schema.string().required(),
+            domainId: Schema.string().default('system'),
+            contestType: Schema.string().default('oi'),
+            dataDir: Schema.string().required(),
+            imageDir: Schema.string().required(),
+        }),
+        (...args) => require('./scripts/poj').run(...args),
     );
 
     ctx.provideModule('hash', 'hust', ($password, $saved) => {
@@ -84,5 +99,6 @@ export function apply(ctx: Context) {
         'migrate from vijos': '从 Vijos 导入',
         'migrate from syzoj': '从 SYZOJ 导入',
         'migrate from universaloj': '从 UniversalOJ 导入',
+        'migrate from poj': '从 POJ 导入',
     });
 }
--- packages/migrate/scripts/poj.ts
+++ packages/migrate/scripts/poj.ts
@@ -1,47 +1,27 @@
 /* eslint-disable no-tabs */
 /* eslint-disable no-await-in-loop */
 import path from 'path';
+import zlib from 'zlib';
 import mariadb from 'mariadb';
-import TurndownService from 'turndown';
 import {
-    _, buildContent, ContestModel, DomainModel, fs, MessageModel, moment, noop, NotFoundError, ObjectId, postJudge, ProblemModel,
-    RecordDoc, RecordModel, SolutionModel, STATUS, StorageModel, SystemModel, Time, UserModel,
+    buildContent, ContestModel, DomainModel, fs, moment, noop, NotFoundError, ObjectId, postJudge, ProblemModel,
+    RecordDoc, RecordModel, SolutionModel, STATUS, SystemModel, Time, UserModel,
 } from 'hydrooj';
 
-const turndown = new TurndownService({
-    codeBlockStyle: 'fenced',
-    bulletListMarker: '-',
-});
-
 const statusMap = {
-    4: STATUS.STATUS_ACCEPTED,
-    5: STATUS.STATUS_WRONG_ANSWER,
-    6: STATUS.STATUS_WRONG_ANSWER,
-    7: STATUS.STATUS_TIME_LIMIT_EXCEEDED,
-    8: STATUS.STATUS_MEMORY_LIMIT_EXCEEDED,
-    9: STATUS.STATUS_OUTPUT_LIMIT_EXCEEDED,
-    10: STATUS.STATUS_RUNTIME_ERROR,
-    11: STATUS.STATUS_COMPILE_ERROR,
+    0: STATUS.STATUS_ACCEPTED,
+    1: STATUS.STATUS_WRONG_ANSWER,
+    2: STATUS.STATUS_TIME_LIMIT_EXCEEDED,
+    3: STATUS.STATUS_MEMORY_LIMIT_EXCEEDED,
+    4: STATUS.STATUS_WRONG_ANSWER,
+    5: STATUS.STATUS_RUNTIME_ERROR,
+    6: STATUS.STATUS_OUTPUT_LIMIT_EXCEEDED,
+    7: STATUS.STATUS_COMPILE_ERROR,
 };
 const langMap = {
-    0: 'c',
-    1: 'cc',
+    0: 'cc',
+    1: 'c',
     2: 'pas',
-    3: 'java',
-    4: 'rb',
-    5: 'bash',
-    6: 'py',
-    7: 'php',
-    8: 'perl',
-    9: 'cs',
-    10: 'oc',
-    11: 'fb',
-    12: 'sc',
-    13: 'cl',
-    14: 'cl++',
-    15: 'lua',
-    16: 'js',
-    17: 'go',
 };
 const nameMap: Record<string, string> = {
     'sample.in': 'sample0.in',
@@ -50,28 +30,19 @@ const nameMap: Record<string, string> = {
     'test.out': 'test0.out',
 };
 
-async function addContestFile(domainId: string, tid: ObjectId, filename: string, filepath: string) {
-    const tdoc = await ContestModel.get(domainId, tid);
-    await StorageModel.put(`contest/${domainId}/${tid}/${filename}`, filepath, 1);
-    const meta = await StorageModel.getMeta(`contest/${domainId}/${tid}/${filename}`);
-    const payload = { _id: filename, name: filename, ..._.pick(meta, ['size', 'lastModified', 'etag']) };
-    if (!meta) return false;
-    await ContestModel.edit(domainId, tid, { files: [...(tdoc.files || []), payload] });
-    return true;
-}
-
 export async function run({
-    host = 'localhost', port = 3306, name = 'jol',
+    host = 'localhost', port = 3306, name,
     username, password, domainId, contestType = 'oi',
-    dataDir, uploadDir = '/home/judge/src/web/upload/', rerun = true, randomMail = false,
+    dataDir, imageDir, rerun = true, randomMail = false,
 }, report: Function) {
-    let remoteUsed = false;
     const src = await mariadb.createConnection({
         host,
         port,
         user: username,
         password,
         database: name,
+        // GBK
+        charset: 'GBK',
     });
     const query = (q: string) => new Promise<any[]>((res, rej) => {
         src.query(q).then((r) => res(r)).catch((e) => rej(e));
@@ -95,30 +66,28 @@ export async function run({
         school      varchar	100	N	用户所在学校
     */
     const uidMap: Record<string, number> = {};
-    const udocs = await query('SELECT * FROM `users`');
+    const udocs = await query('SELECT *, DECODE(`password`, "PWDforJO2005") as `depass` FROM `users`');
     const precheck = await UserModel.getMulti({ unameLower: { $in: udocs.map((u) => u.user_id.toLowerCase()) } }).toArray();
     if (precheck.length) throw new Error(`Conflict username: ${precheck.map((u) => u.unameLower).join(', ')}`);
     for (const udoc of udocs) {
         if (randomMail) delete udoc.email;
-        let current = await UserModel.getByEmail(domainId, udoc.email || `${udoc.user_id}@hustoj.local`);
+        let current = await UserModel.getByEmail(domainId, udoc.email || `${udoc.user_id}@poj.local`);
         current ||= await UserModel.getByUname(domainId, udoc.user_id);
         if (current) {
             report({ message: `duplicate user with email ${udoc.email}: ${current.uname},${udoc.user_id}` });
             uidMap[udoc.user_id] = current._id;
         } else {
             const uid = await UserModel.create(
-                udoc.email || `${udoc.user_id}@hustoj.local`, udoc.user_id, '',
+                udoc.email || `${udoc.user_id}@poj.local`, udoc.user_id, '',
                 null, udoc.ip, udoc.defunct === 'Y' ? 0 : SystemModel.get('default.priv'),
             );
             uidMap[udoc.user_id] = uid;
             await UserModel.setById(uid, {
                 loginat: udoc.accesstime,
                 regat: udoc.reg_time,
-                hash: udoc.password,
-                salt: udoc.password,
                 school: udoc.school || '',
-                hashType: 'hust',
             });
+            await UserModel.setPassword(uid, udoc.depass);
             await DomainModel.setUserInDomain(domainId, uid, {
                 displayName: udoc.nick || '',
                 school: udoc.school || '',
@@ -139,20 +108,26 @@ export async function run({
         description	text		Y	题目描述
         inupt	text		Y	输入说明
         output	text		Y	输出说明
+        input_path
+        output_path
         sample_input	text		Y	输入参照
         sample_output	text		Y	输出参照
-        spj	char	1	N	是否为特别题目
+        sample_Program
         hint	text		Y	暗示
         source	varchar	100	Y	来源
         in_date	datetime		Y	加入时间
-        time_limit	int	11	N	限时（秒）
-        memory_limit	int	11	N	空间限制(MByte)
+        time_limit	int	11	N	限时（ms）
+        memory_limit	int	11	N	空间限制(k)
         defunct	char	1	N	是否屏蔽（Y/N）
+        contest_id  #ingore
         accepted	int	11	Y	总ac次数
         submit	int	11	Y	总提交次数
+        ratio
+        error
+        difficulty
+        submit_user
         solved	int	11	Y	解答（未用）
-
-        solution #optional
+        case_time_limit #ingore
     */
     const pidMap: Record<string, number> = {};
     const [{ 'count(*)': pcount }] = await query('SELECT count(*) FROM `problem`');
@@ -175,13 +150,13 @@ export async function run({
                     hint: pdoc.hint,
                     source: pdoc.source,
                 }, 'html');
-                const uploadFiles = content.matchAll(/(?:src|href)="\/upload\/([^"]+\/([^"]+))"/g);
+                const uploadFiles = content.matchAll(/(?:src|href)="\/images\/([^"]+\/([^"]+))"/g);
                 for (const file of uploadFiles) {
                     try {
-                        files[file[2]] = await fs.readFile(path.join(uploadDir, file[1]));
-                        content = content.replace(`/upload/${file[1]}`, `file://${file[2]}`);
+                        files[file[2]] = await fs.readFile(path.join(imageDir, file[1]));
+                        content = content.replace(`images/${file[1]}`, `file://${file[2]}`);
                     } catch (e) {
-                        report({ message: `failed to read file: ${path.join(uploadDir, file[1])}` });
+                        report({ message: `failed to read file: ${path.join(imageDir, file[1])}` });
                     }
                 }
                 const pid = await ProblemModel.add(
@@ -194,36 +169,18 @@ export async function run({
                 await Promise.all(Object.keys(files).map((filename) => ProblemModel.addAdditionalFile(domainId, pid, filename, files[filename])));
                 if (Object.keys(files).length) report({ message: `move ${Object.keys(files).length} file for problem ${pid}` });
             }
-            const cdoc = await query(`SELECT * FROM \`privilege\` WHERE \`rightstr\` = 'p${pdoc.problem_id}'`);
-            const maintainer = [];
-            for (let i = 1; i < cdoc.length; i++) maintainer.push(uidMap[cdoc[i].user_id]);
             await ProblemModel.edit(domainId, pidMap[pdoc.problem_id], {
                 nAccept: 0,
                 nSubmit: pdoc.submit,
-                config: `time: ${pdoc.time_limit}s
-memory: ${pdoc.memory_limit}m
-${pdoc.remote_oj === 'bas' ? `type: remote_judge
-subType: ybtbas
-target: ybtbas/${+pdoc.id - 3000}
-` : ''}`,
-                owner: uidMap[cdoc[0]?.user_id] || 1,
-                maintainer,
+                config: `time: ${pdoc.time_limit}ms
+memory: ${pdoc.memory_limit}k`,
+                owner: 1,
                 html: true,
             });
-            if (pdoc.remote_oj === 'bas') remoteUsed = true;
-            if (pdoc.solution) {
-                const md = turndown.turndown(pdoc.solution);
-                await SolutionModel.add(domainId, pidMap[pdoc.problem_id], 1, md);
+            if (pdoc.sample_Program) {
+                await SolutionModel.add(domainId, pidMap[pdoc.problem_id], 1, pdoc.sample_Program);
             }
         }
-    }
-    if (remoteUsed) {
-        MessageModel.sendNotification(`您导入的数据中使用了一本通编程启蒙远端测试题目。
-请在迁移脚本运行完成后，在终端中运行
-hydrooj install https://hydro.ac/hydroac-client.zip
-安装远端评测所需要的插件。插件安装完成后，请重启 hydrooj，待一分钟后，再次重启 hydrooj。
-在此之后，远端评测应当能够正常进行。
-（注：您无需再使用申请的评测账号）`);
     }
     report({ message: 'problem finished' });
 
@@ -235,32 +192,19 @@ hydrooj install https://hydro.ac/hydroac-client.zip
         defunct	char	1	N	是否屏蔽（Y/N）
         description	text		Y	描述（在此版本中未用）
         private	tinyint	4		公开/内部（0/1）
-        langmask	int	11		语言
-        password	char(16)			进入比赛的密码
-        user_id	char(48)			允许参加比赛用户列表
     */
     const tidMap: Record<string, string> = {};
     const tdocs = await query('SELECT * FROM `contest`');
     for (const tdoc of tdocs) {
-        const pdocs = await query(`SELECT * FROM \`contest_problem\` WHERE \`contest_id\` = ${tdoc.contest_id}`);
+        const pdocs = await query(`SELECT * FROM \`contest_problem\` WHERE \`contest_id\` = ${tdoc.contest_id} ORDER BY \`num\` ASC`);
         const pids = pdocs.map((i) => pidMap[i.problem_id]).filter((i) => i);
-        const files = {};
-        let description = tdoc.description;
-        const uploadFiles = description.matchAll(/(?:src|href)="\/upload\/([^"]+\/([^"]+))"/g);
-        for (const file of uploadFiles) {
-            files[file[2]] = await fs.readFile(path.join(uploadDir, file[1]));
-            description = description.replace(`/upload/${file[1]}`, `file://${file[2]}`);
-        }
-        // WHY you allow contest with end time BEFORE start time? WHY???
         const endAt = moment(tdoc.end_time).isSameOrBefore(tdoc.start_time) ? moment(tdoc.end_time).add(1, 'minute').toDate() : tdoc.end_time;
         const tid = await ContestModel.add(
-            domainId, tdoc.title, description || 'Description',
+            domainId, tdoc.title, tdoc.description || 'Description',
             adminUids[0], contestType, tdoc.start_time, endAt, pids, true,
-            { _code: password },
+            tdoc.private ? { _code: password } : {},
         );
         tidMap[tdoc.contest_id] = tid.toHexString();
-        await Promise.all(Object.keys(files).map((filename) => addContestFile(domainId, tid, filename, files[filename])));
-        if (Object.keys(files).length) report({ message: `move ${Object.keys(files).length} file for contest ${tidMap[tdoc.contest_id]}` });
     }
     report({ message: 'contest finished' });
     /*
@@ -271,18 +215,15 @@ hydrooj install https://hydro.ac/hydroac-client.zip
         user_id	char	20	N	用户id
         time	int	11	N	用时（秒）
         memory	int	11	N	所用空间（）
+        className
         in_date	datetime		N	加入时间
-        result	smallint	6	N	结果（4：AC）
+        result	smallint	6	N	结果（0：AC）
         language	tinyint	4	N	语言
         ip	char	15	N	用户ip
         contest_id	int	11	Y	所属于竞赛组
         valid	tinyint	4	N	是否有效？？？
         num	tinyint	4	N	题目在竞赛中的顺序号
-        code_lenght	int	11	N	代码长度
-        judgetime	datetime		Y	判题时间
-        pass_rate	decimal	2	N	通过百分比（OI模式下可用）
-        lint_error	int		N	？？？
-        judger	char(16)		N	判题机
+        code_length	int	11	N	代码长度
     */
     const [{ 'count(*)': rcount }] = await query('SELECT count(*) FROM `solution`');
     const rpageCount = Math.ceil(Number(rcount) / step);
@@ -293,11 +234,11 @@ hydrooj install https://hydro.ac/hydroac-client.zip
                 status: statusMap[rdoc.result] || 0,
                 _id: Time.getObjectID(rdoc.in_date, false),
                 uid: uidMap[rdoc.user_id] || 0,
-                code: "HustOJ didn't provide user code",
-                lang: langMap[rdoc.language] || '',
+                code: "POJ didn't provide user code",
+                lang: langMap[rdoc.language || 0],
                 pid: pidMap[rdoc.problem_id] || 0,
                 domainId,
-                score: rdoc.pass_rate ? Math.ceil(rdoc.pass_rate * 100) : rdoc.result === 4 ? 100 : 0,
+                score: rdoc.result === 0 ? 100 : 0,
                 time: rdoc.time || 0,
                 memory: rdoc.memory || 0,
                 judgeTexts: [],
@@ -309,10 +250,9 @@ hydrooj install https://hydro.ac/hydroac-client.zip
             };
             const ceInfo = await query(`SELECT \`error\` FROM \`compileinfo\` WHERE \`solution_id\` = ${rdoc.solution_id}`);
             if (ceInfo[0]?.error) data.judgeTexts.push(ceInfo[0].error);
-            const rtInfo = await query(`SELECT \`error\` FROM \`runtimeinfo\` WHERE \`solution_id\` = ${rdoc.solution_id}`);
-            if (rtInfo[0]?.error) data.judgeTexts.push(rtInfo[0].error);
             const source = await query(`SELECT \`source\` FROM \`source_code\` WHERE \`solution_id\` = ${rdoc.solution_id}`);
-            if (source[0]?.source) data.code = source[0].source;
+            // decompress mysql buffer, slice 4 then it can start at 78 9c (zlib header)
+            if (source[0]?.source) data.code = zlib.inflateSync(source[0].source.slice(4)).toString('utf-8');
             if (rdoc.contest_id) {
                 data.contest = new ObjectId(tidMap[rdoc.contest_id]);
                 await ContestModel.attend(domainId, data.contest, uidMap[rdoc.user_id]).catch(noop);
@@ -337,7 +277,8 @@ hydrooj install https://hydro.ac/hydroac-client.zip
         for (const data of datas) {
             if (data.isDirectory()) continue;
             const filename = nameMap[data.name] || data.name;
-            await ProblemModel.addTestdata(domainId, pdoc.docId, filename, `${dataDir}/${file.name}/${data.name}`);
+            const originalFile = fs.readFileSync(`${dataDir}/${file.name}/${data.name}`).toString();
+            await ProblemModel.addTestdata(domainId, pdoc.docId, filename, originalFile.replace(/\r\n/g, '\n'));
         }
         await ProblemModel.addTestdata(domainId, pdoc.docId, 'config.yaml', Buffer.from(pdoc.config as string));
     }
