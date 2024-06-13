0722cc9e794b604731a34b977963ab67711710ed
undefined <i@undefined.moe>
2024-06-13 16:32:10 +0800

    core: fix problem.getList return type

--- packages/hydrooj/src/model/problem.ts
+++ packages/hydrooj/src/model/problem.ts
@@ -363,7 +363,7 @@ export class ProblemModel {
         domainId: string, pids: number[], canViewHidden: number | boolean = false,
         doThrow = true, projection = ProblemModel.PROJECTION_PUBLIC, indexByDocIdOnly = false,
     ): Promise<ProblemDict> {
-        if (!pids?.length) return [];
+        if (!pids?.length) return {};
         const r: Record<number, ProblemDoc> = {};
         const l: Record<string, ProblemDoc> = {};
         const q: any = { docId: { $in: pids } };
