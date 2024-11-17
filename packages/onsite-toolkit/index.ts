53fc85ddd6c58dc321cee4957a58c9f7cb2f0d0a
undefined <i@undefined.moe>
2024-11-17 23:28:39 +0800

    scoreboard: bug fixes

--- packages/onsite-toolkit/index.ts
+++ packages/onsite-toolkit/index.ts
@@ -55,7 +55,7 @@ export function apply(ctx: Context) {
                     payload: {
                         name: tdoc.title,
                         duration: Math.floor((new Date(tdoc.endAt).getTime() - new Date(tdoc.beginAt).getTime()) / 1000),
-                        frozen: Math.floor((new Date(tdoc.endAt).getTime() - new Date(tdoc.lockAt).getTime()) / 1000),
+                        frozen: Math.floor((new Date(tdoc.lockAt).getTime() - new Date(tdoc.beginAt).getTime()) / 1000),
                         problems: tdoc.pids.map((i, n) => ({ name: pid(n), id: i.toString() })),
                         teams: teams.map((t) => ({
                             id: t.uid.toString(),
