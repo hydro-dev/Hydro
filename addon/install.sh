9e3669ba208a9fd2c137c2846bd93577a58a5ce5
helloxm <fjt0327@163.com>
2024-12-29 22:17:43 +0800

    update

---
+++ addon/install.sh
@@ -0,0 +1,6 @@
+
+
+cp  ../packages/hydrooj/src/lib/ui.ts /usr/local/share/.config/yarn/global/node_modules/hydrooj/src/lib/ -rf
+cp templates/* /root/addon/templates/ -rf
+cp locales/* /root/addon/locales/ -rf 
+cp public/* /root/addon/public/ -rf 
