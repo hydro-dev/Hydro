a45578e88d1ecc43f2125767552307d321ce9690
helloxm <fjt0327@163.com>
2025-01-02 11:28:43 +0800

    update

--- addon/install.sh
+++ addon/install.sh
@@ -1,6 +1,9 @@
 
 
-cp  ../packages/hydrooj/src/lib/ui.ts /usr/local/share/.config/yarn/global/node_modules/hydrooj/src/lib/ -rf
+cp ../packages/hydrooj/src/lib/ui.ts /usr/local/share/.config/yarn/global/node_modules/hydrooj/src/lib/ui.ts -rf
+cp ../packages/hydrooj/src/interface.ts /usr/local/share/.config/yarn/global/node_modules/hydrooj/src/interface.ts -rf
+cp ../packages/hydrooj/src/model/domain.ts /usr/local/share/.config/yarn/global/node_modules/hydrooj/src/model/domain.ts -rf 
+cp ../packages/hydrooj/src/handler/home.ts /usr/local/share/.config/yarn/global/node_modules/hydrooj/src/handler/home.ts -rf 
 cp templates/* /root/addon/templates/ -rf
 cp locales/* /root/addon/locales/ -rf 
 # cp public/* /root/addon/public/ -rf 
