7e21a72d6f010d141975879b5313ad2759c44c9f
helloxm <fjt0327@163.com>
2025-01-07 23:24:29 +0800

    update

--- packages/hydrooj/src/model/setting.ts
+++ packages/hydrooj/src/model/setting.ts
@@ -188,6 +188,7 @@ AccountSetting(
     Setting('setting_info', 'avatar', '', 'text', 'Avatar',
         'Allow using gravatar:email qq:id github:name url:link format.'),
     Setting('setting_info', 'qq', null, 'text', 'QQ'),
+    Setting('setting_info', 'qrcodeurl', null, 'text', 'QrcodeUrl'),
     Setting('setting_info', 'gender', builtin.USER_GENDER_OTHER, builtin.USER_GENDER_RANGE, 'Gender'),
     Setting('setting_info', 'bio', null, 'markdown', 'Bio'),
     Setting('setting_info', 'school', '', 'text', 'School'),
