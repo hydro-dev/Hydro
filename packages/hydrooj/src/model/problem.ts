93e5cf72035b8f5aca84a1c816ef1bc1688b3ef9
panda <panda_dtdyy@outlook.com>
2025-03-26 22:17:29 +0800

    core: fix icpc problem package import

--- packages/hydrooj/src/model/problem.ts
+++ packages/hydrooj/src/model/problem.ts
@@ -8,7 +8,7 @@ import yaml from 'js-yaml';
 import { pick } from 'lodash';
 import { Filter, ObjectId } from 'mongodb';
 import type { Readable } from 'stream';
-import { ProblemConfigFile } from '@hydrooj/common';
+import { ProblemConfigFile, ProblemType } from '@hydrooj/common';
 import { Logger, size, streamToBuffer } from '@hydrooj/utils/lib/utils';
 import { Context } from '../context';
 import { FileUploadError, ProblemNotFoundError, ValidationError } from '../error';
@@ -41,6 +41,7 @@ function sortable(source: string, namespaces: Record<string, string>) {
 }
 
 function findOverrideContent(dir: string, base: string) {
+    if (!fs.existsSync(dir)) return null;
     let files = fs.readdirSync(dir);
     if (files.includes(`${base}.md`)) return fs.readFileSync(path.join(dir, `${base}.md`), 'utf8');
     if (files.includes(`${base}.pdf`)) return `@[PDF](file://${base}.pdf)`;
@@ -478,6 +479,11 @@ export class ProblemModel {
                 throw new ValidationError('file', null, 'Invalid file');
             }
             const files = await fs.readdir(tmpdir, { withFileTypes: true });
+            if (files.find((f) => f.name === 'problem.yaml')) {
+                problems = ['.']; // special case for ICPC problem package
+            } else {
+                problems = files.filter((f) => f.isDirectory()).map((i) => i.name);
+            }
             problems = files.filter((f) => f.isDirectory()).map((i) => i.name);
         } catch (e) {
             if (options.delSource) await fs.remove(tmpdir);
@@ -549,6 +555,7 @@ export class ProblemModel {
                 if ((pdoc as any).limits) {
                     config.time = (pdoc as any).limits.time_limit;
                     config.memory = (pdoc as any).limits.memory;
+                    configChanged = true;
                 }
                 const docId = overridePid
                     ? (await ProblemModel.edit(domainId, overridePid, {
@@ -575,6 +582,22 @@ export class ProblemModel {
                         await (f.name === 'sample' ? ProblemModel.addAdditionalFile
                             : f.name === 'secret' ? ProblemModel.addTestdata
                                 : null)?.(domainId, docId, file, path.join(loc, file));
+                    }
+                }
+                for (const [f, loc] of await getFiles('output_validators')) {
+                    if (!f.isFile()) continue;
+                    const sub = await fs.readdir(loc);
+                    for (const file of sub) {
+                        if (file === 'testlib.h') continue;
+                        await ProblemModel.addTestdata(domainId, docId, file, path.join(loc, file));
+                        if (file === 'checker') {
+                            config.checker_type = 'testlib';
+                            config.checker = file;
+                        } else if (file === 'interactor') {
+                            config.type = ProblemType.Interactive;
+                            config.interactor = file;
+                        }
+                        configChanged = true;
                     }
                 }
                 for (const [f, loc] of await getFiles('additional_file', 'attachments', 'statement', 'problem_statement')) {
