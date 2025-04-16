083b5b6641988b26d4caf713f58ff09e6e7ca43b
undefined <i@undefined.moe>
2025-04-16 10:32:04 +0800

    ui: add join domain button

---
+++ packages/ui-default/pages/home_domain.page.tsx
@@ -0,0 +1,41 @@
+import $ from 'jquery';
+import DomainSelectAutoComplete from 'vj/components/autocomplete/DomainSelectAutoComplete';
+import { ActionDialog } from 'vj/components/dialog';
+import { NamedPage } from 'vj/misc/Page';
+import { i18n, tpl } from 'vj/utils';
+
+export default new NamedPage('home_domain', () => {
+  $(tpl(
+    <div style={{ display: 'none' }} className="dialog__body--join-domain" id="join-domain-dialog">
+      <div className="row"><div className="columns">
+        <h1>{i18n('Join Domain')}</h1>
+      </div></div>
+      <div className="row">
+        <div className="columns">
+          <label>
+            {i18n('Domain ID')}
+            <div className="textbox-container">
+              <input name="domainId" type="text" className="textbox" data-autofocus />
+            </div>
+          </label>
+        </div>
+      </div>
+    </div>,
+  )).appendTo(document.body);
+  const selector: any = DomainSelectAutoComplete.getOrConstruct($('#join-domain-dialog [name="domainId"]'));
+
+  $(document).on('click', '[id="join-domain-button"]', async () => {
+    const action = await new ActionDialog({
+      $body: $('.dialog__body--join-domain > div'),
+      onDispatch(a) {
+        if (a === 'ok' && selector.value() === null) {
+          selector.focus();
+          return false;
+        }
+        return true;
+      },
+    }).open();
+    if (action !== 'ok') return;
+    window.location.href = `/domain/join?target=${encodeURIComponent(selector.value()._id.toString())}`;
+  });
+});
--- packages/ui-default/pages/problem_main.page.ts
+++ packages/ui-default/pages/problem_main.page.ts
@@ -184,23 +184,6 @@ async function handleOperation(operation) {
     }).open();
     if (action !== 'yes') return;
   } else if (operation === 'copy') {
-    $(tpl`
-      <div style="display: none" class="dialog__body--problem-copy">
-        <div class="row"><div class="columns">
-          <h1 name="select_user_hint">${i18n('Copy Problems')}</h1>
-        </div></div>
-        <div class="row">
-          <div class="columns">
-            <label>
-              ${i18n('Target')}
-              <div class="textbox-container">
-                <input name="target" type="text" class="textbox" data-autofocus>
-              </div>
-            </label>
-          </div>
-        </div>
-      </div>
-    `).appendTo(document.body);
     const domainSelector: any = DomainSelectAutoComplete.getOrConstruct($('.dialog__body--problem-copy [name="target"]'));
     const copyDialog = await new ActionDialog({
       $body: $('.dialog__body--problem-copy > div'),
@@ -303,6 +286,23 @@ const page = new NamedPage(['problem_main'], () => {
   const $body = $('body');
   $body.addClass('display-mode');
   $('.section.display-mode').removeClass('display-mode');
+  $(tpl`
+    <div style="display: none" class="dialog__body--problem-copy">
+      <div class="row"><div class="columns">
+        <h1 name="select_user_hint">${i18n('Copy Problems')}</h1>
+      </div></div>
+      <div class="row">
+        <div class="columns">
+          <label>
+            ${i18n('Target')}
+            <div class="textbox-container">
+              <input name="target" type="text" class="textbox" data-autofocus>
+            </div>
+          </label>
+        </div>
+      </div>
+    </div>
+  `).appendTo(document.body);
 
   buildSearchContainer();
   buildLegacyCategoryFilter();
