39cc430969efa8d816e8eefa6b735c011971b7ff
panda <panda_dtdyy@outlook.com>
2025-05-27 10:05:46 +0000

    add manage_rejudge

--- packages/ui-default/pages/manage_rejudge.page.ts
+++ packages/ui-default/pages/manage_rejudge.page.ts
@@ -1,3 +1,4 @@
+import { STATUS_TEXTS } from '@hydrooj/common';
 import $ from 'jquery';
 import CustomSelectAutoComplete from 'vj/components/autocomplete/CustomSelectAutoComplete';
 import ProblemSelectAutoComplete from 'vj/components/autocomplete/ProblemSelectAutoComplete';
@@ -15,7 +16,9 @@ const page = new NamedPage('manage_rejudge', async () => {
   const langs = Object.keys(window.LANGS).filter((i) => !prefixes.has(i)).map((i) => (
     { name: `${i.includes('.') ? `${window.LANGS[i.split('.')[0]].display}/` : ''}${window.LANGS[i].display}`, _id: i }
   ));
-  CustomSelectAutoComplete.getOrConstruct($('[name=lang]'), { multi: false, data: langs });
+  CustomSelectAutoComplete.getOrConstruct($('[name=lang]'), { multi: true, data: langs });
+  const statuses = Object.values(STATUS_TEXTS).map((i) => ({ name: i, _id: i }));
+  CustomSelectAutoComplete.getOrConstruct($('[name=status]'), { multi: true, data: statuses });
 });
 
 export default page;
