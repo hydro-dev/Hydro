05c3b8f2bdd71835ca6e63960b06fb3ad7f25427
undefined <i@undefined.moe>
2025-08-05 03:03:01 +0800

    ui: fix selectedKeys

--- packages/ui-default/components/autocomplete/components/LanguageSelectAutoComplete.tsx
+++ packages/ui-default/components/autocomplete/components/LanguageSelectAutoComplete.tsx
@@ -11,11 +11,12 @@ const LanguageSelectAutoComplete = React.forwardRef<any, any>((props, ref) => (
   <CustomSelectAutoComplete
     ref={ref as any}
     {...props}
+    selectedKeys={typeof props.selectedKeys === 'string' ? props.selectedKeys.split(',') : props.selectedKeys}
     data={props.withAuto ? withAuto : data}
     renderItem={(item) => (prefixes.has(item._id) ? '' : `${item.name}`)}
     onChange={(val) => {
       let value = val.split(',');
-      const active = new Set(value.filter((i) => i.includes('.').map((i) => i.split('.')[0])));
+      const active = new Set(value.filter((i) => i.includes('.')).map((i) => i.split('.')[0]));
       value = value.filter((i) => !prefixes.has(i) || active.has(i));
       for (const i of active) if (!value.includes(i)) value.push(i);
       props.onChange(value.join(','));
