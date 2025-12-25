f2a5a17f95bd8bd8858aafbc7f52646a085e48d4
Baoshuo <i@baoshuo.ren>
2025-12-25 13:42:49 +0800

    ui-next: add hydroPlugins to dynamically load route plugins

--- packages/ui-next/index.ts
+++ packages/ui-next/index.ts
@@ -1,13 +1,36 @@
 import fs from 'fs';
 import path from 'path';
-import importMetaUrlPlugin from '@codingame/esbuild-import-meta-url-plugin';
-import react from '@vitejs/plugin-react-swc';
+import importMetaUrlPlugin from '@chialab/esbuild-plugin-meta-url';
+import react from '@vitejs/plugin-react';
 import c2k from 'koa2-connect/ts';
-import { createServer } from 'vite';
+import { createServer, type Plugin } from 'vite';
 import { } from '@hydrooj/framework';
-import { Context } from 'hydrooj';
+import { Context, getNodes } from 'hydrooj';
 
 const INJECT_MARKER = '<!-- __HYDRO_INJECTION__DO_NOT_REMOVE_THIS__ -->';
+
+function hydroPlugins(): Plugin {
+    const virtualModuleId = 'virtual:hydro-plugins';
+    const resolvedVirtualModuleId = `\0${virtualModuleId}`;
+
+    return {
+        name: 'hydro-plugins',
+        resolveId(id) {
+            if (id === virtualModuleId) {
+                return resolvedVirtualModuleId;
+            }
+        },
+        load(id) {
+            if (id === resolvedVirtualModuleId) {
+                const plugins = getNodes('Route');
+                if (!plugins) return 'export default [];';
+                const imports = plugins.map((p, i) => `import plugin${i} from '${p.args.entry}';`).join('\n');
+                const exports = `export default [${plugins.map((p, i) => `plugin${i}`).join(', ')}];`;
+                return `${imports}\n${exports}`;
+            }
+        },
+    };
+}
 
 export async function apply(ctx: Context) {
     if (process.env.HYDRO_CLI) return;
@@ -26,7 +49,7 @@ export async function apply(ctx: Context) {
         appType: 'custom',
         root: __dirname,
         base: '/',
-        plugins: [react()],
+        plugins: [react(), hydroPlugins()],
         optimizeDeps: {
             esbuildOptions: {
                 plugins: [
