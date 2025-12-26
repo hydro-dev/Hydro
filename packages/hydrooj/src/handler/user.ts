cd94929e20dce7a9f97698b74ae409fb1a9b52de
Baoshuo <i@baoshuo.ren>
2025-12-26 22:51:07 +0800

    core: fix oauth callback with mail only (#1110)

--- packages/hydrooj/src/handler/user.ts
+++ packages/hydrooj/src/handler/user.ts
@@ -476,9 +476,16 @@ class OauthCallbackHandler extends Handler {
             return;
         }
 
-        const uid = await this.ctx.oauth.get(args.type, r._id) || await this.ctx.oauth.get('mail', r.email);
+        const uid = await this.ctx.oauth.get(args.type, r._id);
         if (uid) {
             await successfulAuth.call(this, await user.getById('system', uid));
+            this.response.redirect = '/';
+            return;
+        }
+        const udoc = await user.getByEmail('system', r.email);
+        if (udoc) {
+            await this.ctx.oauth.set(args.type, r._id, udoc._id);
+            await successfulAuth.call(this, udoc);
             this.response.redirect = '/';
             return;
         }
