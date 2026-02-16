be242479fa8443192f9582ec399531d2a6c4ece2
undefined <i@undefined.moe>
2026-02-16 13:47:46 +0800

    upgrade deps

--- packages/ui-default/components/messagepad/MessagePadDialogueContentContainer.jsx
+++ packages/ui-default/components/messagepad/MessagePadDialogueContentContainer.jsx
@@ -1,4 +1,3 @@
-import 'jquery-scroll-lock';
 import 'jquery.easing';
 
 import $ from 'jquery';
@@ -24,13 +23,12 @@ export default connect(mapStateToProps)(class MessagePadDialogueContentContainer
     if (this.props.activeId !== prevProps.activeId) {
       this.scrollToBottom = true;
       this.scrollWithAnimation = false;
-    } else if (node.scrollTop + node.offsetHeight === node.scrollHeight) {
+    } else if (Math.abs(node.scrollTop + node.offsetHeight - node.scrollHeight) < 200) {
       this.scrollToBottom = true;
       this.scrollWithAnimation = true;
     } else this.scrollToBottom = false;
 
     if (!node) return;
-    $(this.state.ref).scrollLock({ strict: true });
     if (this.scrollToBottom) {
       const targetScrollTop = node.scrollHeight - node.offsetHeight;
       if (this.scrollWithAnimation) {
@@ -98,7 +96,7 @@ export default connect(mapStateToProps)(class MessagePadDialogueContentContainer
             </a>
           )}
         </div>
-        <ol className="messagepad__content" ref={(ref) => { this.setState({ ...this.state, ref }); }}>
+        <ol className="messagepad__content" style={{ overscrollBehavior: 'contain' }} ref={(ref) => { this.setState({ ...this.state, ref }); }}>
           {this.renderInner()}
         </ol>
       </>
