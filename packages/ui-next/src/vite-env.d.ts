2fe8978b1c2740c304ff3bdcd206e8730b3d0ff3
Baoshuo <i@baoshuo.ren>
2026-04-24 23:20:10 +0800

    ui-next: fix vite-env.d.ts typing

--- packages/ui-next/src/vite-env.d.ts
+++ packages/ui-next/src/vite-env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 declare module 'virtual:hydro-plugins' {
-    import type { Context } from './context';
-    const plugins: Array<{ apply: (ctx: Context) => void }>;
+    import type { RegistryContext } from './registry';
+    const plugins: Array<{ apply: (ctx: RegistryContext) => void }>;
     export default plugins;
 }
