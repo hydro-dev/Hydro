7bb29b8c2a718e410020fb17c8ba8cec666d9a47
undefined <i@undefined.moe>
2026-05-03 11:48:11 +0800

    judge: fix cac load (close #1154)

--- packages/hydrojudge/bin/hydrojudge.js
+++ packages/hydrojudge/bin/hydrojudge.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const argv = require('cac')().parse();
+const argv = require('cac')().cac().parse();
 require('@hydrooj/register');
 
 if (argv.args[0] === 'cache') require('../src/cache')();
