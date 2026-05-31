7e747ad46ca66046888320cfeb727b5bf1fedcc9
panda <panda_dtdyy@outlook.com>
2026-05-31 19:48:25 +0000

    add problem type check

--- packages/hydrojudge/src/cases.ts
+++ packages/hydrojudge/src/cases.ts
@@ -38,6 +38,9 @@ function isValidConfig(config) {
     if (config.multi_pass && (!Number.isInteger(config.multi_pass) || config.multi_pass < 2 || config.multi_pass > 20)) {
         throw new FormatError('Multi Pass must be between 2 and 20.');
     }
+    if (config.multi_pass > 1 && !['default', 'interactive'].includes(config.type)) {
+        throw new FormatError('Multi Pass only supported on default and interactive problems.');
+    }
     if (config.multi_pass > 1 && config.type === 'default' && !['testlib', 'kattis'].includes(config.checker_type)) {
         throw new FormatError('Multi Pass on default problems requires a testlib or kattis checker.');
     }
